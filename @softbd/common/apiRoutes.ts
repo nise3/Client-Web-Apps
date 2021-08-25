534455810ea1cde3499e91286481ceaebd2d7040
Hasan <hasan.softbdltd@gmail.com>
2021-08-25 12:11:59 +0600

    gateway url updating.

--- @softbd/common/apiRoutes.ts
+++ @softbd/common/apiRoutes.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://api.bus.softbd.xyz';
-export const CORE_SERVICE_PATH = '/core/api/v1';
-export const ORGANIZATION_SERVICE_PATH = '/org/api/v1';
-export const INSTITUTE_SERVICE_PATH = '/institute/api/v1';
+export const API_BASE_URL = 'https://gateway.bus.softbd.xyz';
+export const CORE_SERVICE_PATH = '/core';
+export const ORGANIZATION_SERVICE_PATH = '/org';
+export const INSTITUTE_SERVICE_PATH = '/institute';
