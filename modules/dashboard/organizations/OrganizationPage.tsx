105bd7f14ec58f3527b6378ace3f6b2bfcafedab
Hasan <hasan.softbdltd@gmail.com>
2021-08-31 18:22:04 +0600

    fix toggle state.

--- modules/dashboard/organizations/OrganizationPage.tsx
+++ modules/dashboard/organizations/OrganizationPage.tsx
@@ -133,7 +133,7 @@ const OrganizationPage = () => {
     urlPath: ORGANIZATION_SERVICE_PATH + '/organizations',
     dataAccessor: 'data',
   });
-  console.log(isToggleTable);
+
   return (
     <>
       <PageBlock
