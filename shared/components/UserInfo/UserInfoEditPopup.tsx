2d0297109a9cef839d067ea2b83880053a1b02fe
AfrarJahin <afrarjahin@gmail.com>
2021-09-22 16:08:22 +0600

    profileImage Upload  added

--- shared/components/UserInfo/UserInfoEditPopup.tsx
+++ shared/components/UserInfo/UserInfoEditPopup.tsx
@@ -1,13 +1,23 @@
 import {SubmitHandler, useForm} from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup';
+// import {yupResolver} from '@hookform/resolvers/yup';
 import HookFormMuiModal from '../../../@softbd/modals/HookFormMuiModal/HookFormMuiModal';
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import IntlMessages from '../../../@crema/utility/IntlMessages';
 import SubmitButton from '../../../@softbd/elements/button/SubmitButton/SubmitButton';
 import Grid from '@material-ui/core/Grid';
 import CustomTextInput from '../../../@softbd/elements/input/CustomTextInput/CustomTextInput';
 import {useIntl} from 'react-intl';
 import IconUser from '../../../@softbd/icons/IconUser';
+import Avatar from '@material-ui/core/Avatar';
+import {makeStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  ProfileImage: {
+    height: '200px',
+    width: '200px',
+  },
+});
 
 interface UserInfoEditPopupProps {
   onClose: () => void;
@@ -16,17 +26,33 @@ interface UserInfoEditPopupProps {
 const UserInfoEditPopup: FC<UserInfoEditPopupProps> = ({
                                                          ...props
                                                        }) => {
-  const {messages} = useIntl();
 
+  const [profileImage, setProfileImage] = useState<any>('/images/userPageImages/profileImage.jpeg');
+  const {messages} = useIntl();
+  const classes = useStyles();
+
+
+  const imageHandler = (event: any) => {
+    console.log(event);
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader.readyState == 2) {
+        setProfileImage(reader.result);
+        console.log(reader.result);
+      }
+    };
+    reader.readAsDataURL(event.target.files[0]);
+
+  };
   const {
     register,
     handleSubmit,
     formState: {errors, isSubmitting},
-  } = useForm<any>({
-    resolver: yupResolver(),
-  });
+  } = useForm<any>();
 
   const isLoading = false;
+
   const onSubmit: SubmitHandler<any> = async (data: []) => {
 
   };
@@ -37,7 +63,7 @@ const UserInfoEditPopup: FC<UserInfoEditPopupProps> = ({
       open={true}
       title={
         <>
-          <IconUser/>
+          <IconUser />
           <IntlMessages
             id='common.edit'
             values={{subject: <IntlMessages id='user.label' />}}
@@ -48,10 +74,25 @@ const UserInfoEditPopup: FC<UserInfoEditPopupProps> = ({
       handleSubmit={handleSubmit(onSubmit)}
       actions={
         <>
-          <SubmitButton isSubmitting={isSubmitting}  />
+          <SubmitButton isSubmitting={isSubmitting} />
         </>
       }>
       <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <Avatar className={classes.ProfileImage} src={profileImage} />
+          <label htmlFor='contained-button-file'>
+            <Button variant='contained' color='primary' component='label'>
+              Upload
+              <input
+                type='file'
+                accept='image/*'
+                onChange={imageHandler}
+                hidden
+
+              />
+            </Button>
+          </label>
+        </Grid>
         <Grid item xs={6}>
           <CustomTextInput
             id='title_en'
