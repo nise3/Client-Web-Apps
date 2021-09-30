8957a965cf454c61b6a8ab517ae20b105560be1c
Animesh Karmakar <animesh.pust@gmail.com>
2021-09-30 16:30:45 +0600

    add job experience popup v.2

--- modules/youthProfile/JobExperienceAddEditPopup.tsx
+++ modules/youthProfile/JobExperienceAddEditPopup.tsx
@@ -75,122 +75,139 @@ const JobExperienceAddEditPopup: FC<JobExperienceAddEditPopupProps> = ({
     resolver: yupResolver(validationSchema),
   });
 
-  useEffect(() => {
-    if (itemData) {
-      reset({
-        title_en: itemData?.title_en,
-        title_bn: itemData?.title_bn,
-        organization_id: itemData?.organization_id,
-        description: itemData?.description,
-        row_status: String(itemData?.row_status),
-      });
-    } else {
-      reset(initialValues);
-    }
-  }, [itemData]);
+  let itemData = {};
 
-  const onSubmit: SubmitHandler<RankType> = async (data: RankType) => {
-    const response = itemId
-      ? await updateRankType(itemId, data)
-      : await createRankType(data);
-    if (isResponseSuccess(response) && isEdit) {
-      successStack(
-        <IntlMessages
-          id='common.subject_updated_successfully'
-          values={{subject: <IntlMessages id='rank_types.label' />}}
-        />,
-      );
-      mutateRankType();
-      props.onClose();
-    } else if (isResponseSuccess(response) && !isEdit) {
-      successStack(
-        <IntlMessages
-          id='common.subject_created_successfully'
-          values={{subject: <IntlMessages id='rank_types.label' />}}
-        />,
-      );
-      props.onClose();
-    } else if (isValidationError(response)) {
-      setServerValidationErrors(response.errors, setError, validationSchema);
-    }
-  };
+  if (itemId) {
+    itemData = {
+      company_name: 'softbd ltd',
+      position: 'software engineer',
+      type_of_employee: 'full time',
+      location: 'dhaka 1232',
+      job_description: 'building web apps',
+      start_date: '12 oct 2993',
+      end_date: '12 oct 2993',
+    };
+  }
 
-  return (
-    <HookFormMuiModal
-      open={true}
-      {...props}
-      title={
-        <>
-          <IconRankType />
-          {isEdit ? (
-            <IntlMessages
-              id='common.edit'
-              values={{subject: <IntlMessages id='rank_types.label' />}}
-            />
-          ) : (
-            <IntlMessages
-              id='common.add_new'
-              values={{subject: <IntlMessages id='rank_types.label' />}}
-            />
-          )}
-        </>
-      }
-      maxWidth={'sm'}
-      handleSubmit={handleSubmit(onSubmit)}
-      actions={
-        <>
-          <CancelButton onClick={props.onClose} isLoading={isLoading} />
-          <SubmitButton isSubmitting={isSubmitting} isLoading={isLoading} />
-        </>
-      }>
-      <Grid container spacing={5}>
-        <Grid item xs={6}>
-          <CustomTextInput
-            id='title_en'
-            label={messages['common.title_en']}
-            register={register}
-            errorInstance={errors}
-            isLoading={isLoading}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <CustomTextInput
-            id='title_bn'
-            label={messages['common.title_bn']}
-            register={register}
-            errorInstance={errors}
-            isLoading={isLoading}
-          />
-        </Grid>
-        {authUser && isNeedToSelectOrganization(authUser) && (
-          <Grid item xs={6}>
-            <CustomFormSelect
-              id='organization_id'
-              label={messages['organization.label']}
-              isLoading={isLoadingOrganizations}
-              control={control}
-              options={organizations}
-              optionValueProp={'id'}
-              optionTitleProp={['title_en', 'title_bn']}
-              errorInstance={errors}
-            />
-          </Grid>
-        )}
-
-        <Grid item xs={6}>
-          <CustomTextInput
-            id='description'
-            label={messages['common.description']}
-            register={register}
-            errorInstance={errors}
-            isLoading={isLoading}
-            multiline={true}
-            rows={3}
-          />
-        </Grid>
-      </Grid>
-    </HookFormMuiModal>
-  );
+  // useEffect(() => {
+  //   if (itemData) {
+  //     reset({
+  //       company_name: itemData?.company_name,
+  //       position: itemData?.position,
+  //       type_of_employee: itemData?.type_of_employee,
+  //       location: itemData?.location,
+  //       job_description: itemData?.job_description,
+  //       start_date: itemData?.start_date,
+  //       end_date: itemData?.end_date,
+  //     });
+  //   } else {
+  //     reset(initialValues);
+  //   }
+  // }, [itemData]);
+  //
+  // const onSubmit: SubmitHandler<RankType> = async (data: RankType) => {
+  //   const response = itemId
+  //     ? await updateRankType(itemId, data)
+  //     : await createRankType(data);
+  //   if (isResponseSuccess(response) && isEdit) {
+  //     successStack(
+  //       <IntlMessages
+  //         id='common.subject_updated_successfully'
+  //         values={{subject: <IntlMessages id='rank_types.label' />}}
+  //       />,
+  //     );
+  //     mutateRankType();
+  //     props.onClose();
+  //   } else if (isResponseSuccess(response) && !isEdit) {
+  //     successStack(
+  //       <IntlMessages
+  //         id='common.subject_created_successfully'
+  //         values={{subject: <IntlMessages id='rank_types.label' />}}
+  //       />,
+  //     );
+  //     props.onClose();
+  //   } else if (isValidationError(response)) {
+  //     setServerValidationErrors(response.errors, setError, validationSchema);
+  //   }
+  // };
+  //
+  // return (
+  //   <HookFormMuiModal
+  //     open={true}
+  //     {...props}
+  //     title={
+  //       <>
+  //         <IconRankType />
+  //         {isEdit ? (
+  //           <IntlMessages
+  //             id='common.edit'
+  //             values={{subject: <IntlMessages id='rank_types.label' />}}
+  //           />
+  //         ) : (
+  //           <IntlMessages
+  //             id='common.add_new'
+  //             values={{subject: <IntlMessages id='rank_types.label' />}}
+  //           />
+  //         )}
+  //       </>
+  //     }
+  //     maxWidth={'sm'}
+  //     handleSubmit={handleSubmit(onSubmit)}
+  //     actions={
+  //       <>
+  //         <CancelButton onClick={props.onClose} isLoading={isLoading} />
+  //         <SubmitButton isSubmitting={isSubmitting} isLoading={isLoading} />
+  //       </>
+  //     }>
+  //     <Grid container spacing={5}>
+  //       <Grid item xs={6}>
+  //         <CustomTextInput
+  //           id='title_en'
+  //           label={messages['common.title_en']}
+  //           register={register}
+  //           errorInstance={errors}
+  //           isLoading={isLoading}
+  //         />
+  //       </Grid>
+  //       <Grid item xs={6}>
+  //         <CustomTextInput
+  //           id='title_bn'
+  //           label={messages['common.title_bn']}
+  //           register={register}
+  //           errorInstance={errors}
+  //           isLoading={isLoading}
+  //         />
+  //       </Grid>
+  //       {authUser && isNeedToSelectOrganization(authUser) && (
+  //         <Grid item xs={6}>
+  //           <CustomFormSelect
+  //             id='organization_id'
+  //             label={messages['organization.label']}
+  //             isLoading={isLoadingOrganizations}
+  //             control={control}
+  //             options={organizations}
+  //             optionValueProp={'id'}
+  //             optionTitleProp={['title_en', 'title_bn']}
+  //             errorInstance={errors}
+  //           />
+  //         </Grid>
+  //       )}
+  //
+  //       <Grid item xs={6}>
+  //         <CustomTextInput
+  //           id='description'
+  //           label={messages['common.description']}
+  //           register={register}
+  //           errorInstance={errors}
+  //           isLoading={isLoading}
+  //           multiline={true}
+  //           rows={3}
+  //         />
+  //       </Grid>
+  //     </Grid>
+  //   </HookFormMuiModal>
+  // );
+  return <></>;
 };
 
 export default JobExperienceAddEditPopup;
