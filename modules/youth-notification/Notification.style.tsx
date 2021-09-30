35960f36f3f8d0734dd3e1120ca51ba89a523f69
AfrarJahin <afrarjahin@gmail.com>
2021-09-30 16:26:09 +0600

    on process

---
+++ modules/youth-notification/Notification.style.tsx
@@ -0,0 +1,102 @@
+import {makeStyles} from '@material-ui/styles';
+import {CremaTheme} from '../../types/AppContextPropsType';
+import {createStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: CremaTheme) =>
+  createStyles({
+    root: {},
+    midBox: {
+      paddingTop: 20,
+    },
+    searchBox: {
+      padding: '10px 5px 5px',
+      alignItems: 'center',
+      marginTop: 10,
+    },
+    searchInputBorderHide: {
+      '& fieldset': {
+        border: 'none',
+      },
+      '& input': {
+        padding: '14px 0px',
+      },
+    },
+    searchButton: {
+      color: '#fff',
+      padding: '8px 14px',
+      width: '95%',
+    },
+    crPopover: {
+      '& .MuiPopover-paper': {
+        width: 260,
+        [theme.breakpoints.up('sm')]: {
+          width: 300,
+        },
+        [theme.breakpoints.up('xl')]: {
+          width: 380,
+        },
+      },
+      '& .scroll-submenu': {
+        maxHeight: 200,
+        [theme.breakpoints.up('xl')]: {
+          maxHeight: 380,
+        },
+      },
+    },
+    btnPopover: {
+      borderRadius: 0,
+      width: '100%',
+      textTransform: 'capitalize',
+    },
+    notiBtn: {
+      justifyContent: 'flex-start',
+      width: '100%',
+      height: 56,
+      fontSize: 16,
+      borderRadius: 0,
+      paddingLeft: '1rem',
+      paddingRight: '1rem',
+      color: theme.palette.grey[800],
+      '&:hover, &:focus': {
+        color: theme.palette.text.primary,
+        backgroundColor: 'transparent',
+      },
+      [theme.breakpoints.up('sm')]: {
+        height: 70,
+      },
+      [theme.breakpoints.up('md')]: {
+        justifyContent: 'center',
+        width: 'auto',
+        borderLeft: 'solid 1px',
+        borderLeftColor: theme.palette.grey[200],
+        color: theme.palette.grey[400],
+        '&:hover, &:focus': {
+          color: theme.palette.text.primary,
+        },
+      },
+      [theme.breakpoints.up('lg')]: {
+        paddingLeft: '1.5rem',
+        paddingRight: '1.5rem',
+      },
+      [theme.breakpoints.up('xl')]: {
+        paddingLeft: '2.5rem',
+        paddingRight: '2.5rem',
+      },
+    },
+    notiIcon: {
+      fontSize: 22,
+      color: theme.palette.text.secondary,
+      [theme.breakpoints.up('xl')]: {
+        fontSize: 30,
+      },
+    },
+    list: {
+      paddingTop: 0,
+      paddingBottom: 0,
+    },
+    badge: {
+      marginRight: 8,
+    },
+  }),
+);
+export default useStyles;
