ee9e9e1caa71ae93867569e13d8c45fcb7279898
Milton Deb <miltondeb.cse.bgc@gmail.com>
2021-10-10 14:28:43 +0600

    app token change

--- @softbd/libs/axiosInstance.ts
+++ @softbd/libs/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {API_BASE_URL} from '../common/apiRoutes';
 import {Cookies} from 'react-cookie';
 import {COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA} from '../../shared/constants/AppConst';
-import token from '../common/appToken';
+import apiAccessToken from '../common/appToken';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -16,22 +16,6 @@ axiosInstance.interceptors.request.use(
     const authAccessTokenData = cookies.get(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
     const userAccessToken = authAccessTokenData?.access_token;
     console.log('userAccessToken', userAccessToken);
-    let apiAccessToken = '';
-    /**
-     * For development purpose. It should be commented in production mode
-     */
-
-    let urlPath = config.url?.split('/')[1];
-
-    if (urlPath == 'institute') {
-      apiAccessToken = token.instituteApi;
-    } else if (urlPath == 'core') {
-      apiAccessToken = token.coreApi;
-    } else if (urlPath == 'org') {
-      apiAccessToken = token.orgApi;
-    } else if (urlPath == 'youth') {
-      apiAccessToken = token.youthApi;
-    }
 
     config.headers = {
       /*Token: `Bearer ${apiAccessToken}`,*/
