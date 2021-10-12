a7c60df8622f9858ed45736aba913fa9eaed7f4d
shakib77 <shakibshaon77@gmail.com>
2021-10-12 19:12:27 +0600

    institute courses page is refactored

--- modules/institute/courses/index.tsx
+++ modules/institute/courses/index.tsx
@@ -6,99 +6,65 @@ import {
   Paper,
   InputBase,
   IconButton,
+  Pagination,
   Container,
+  Chip,
 } from '@mui/material';
-import {styled} from '@mui/material/styles';
-import usePagination from '@mui/material/usePagination';
 import CustomFormSelect from '../../../@softbd/elements/input/CustomFormSelect/CustomFormSelect';
 import React, {useEffect, useMemo, useState} from 'react';
 import {useForm} from 'react-hook-form';
 import {useIntl} from 'react-intl';
 import {yupResolver} from '@hookform/resolvers/yup';
 import SearchIcon from '@mui/icons-material/Search';
-import makeStyles from '@mui/styles/makeStyles';
 import CourseCardComponent from '../../../@softbd/elements/CourseCardComponent';
 import yup from '../../../@softbd/libs/yup';
 import {H2} from '../../../@softbd/elements/common';
 
-const List = styled('ul')({
-  listStyle: 'none',
-  padding: 0,
-  margin: 0,
-  display: 'flex',
-});
-
-const useStyles = makeStyles((theme) => {
-  return {
-    typographyNumber: {
-      color: theme.palette.primary.dark,
-      marginLeft: '5px',
-      backgroundColor: theme.palette.primary.light,
-      padding: '0 5px',
-    },
-    cardMainGrid: {
-      marginRight: 'auto',
-      marginLeft: 'auto',
-    },
-    filterMainGrid: {
-      marginRight: 'auto',
-      marginLeft: 'auto',
-      justifyContent: 'space-between',
-    },
-  };
-});
-
+const courseList = [
+  {
+    id: 1,
+    image: '/images/popular-course1.png',
+    title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
+    fee: '5,000',
+    providerLogo: '/images/creative_it.jpeg',
+    providerName: 'Diane Croenwett',
+    createDate: 'Mar 19,2020',
+    tags: ['2hr, 47 min', '24 lessons'],
+  },
+  {
+    id: 2,
+    image: '/images/popular-course1.png',
+    title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
+    fee: '5,000',
+    providerLogo: '/images/creative_it.jpeg',
+    providerName: 'Diane Croenwett',
+    createDate: 'Mar 19,2020',
+    tags: ['2hr, 47 min', '24 lessons'],
+  },
+  {
+    id: 3,
+    image: '/images/popular-course1.png',
+    title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
+    fee: '5,000',
+    providerLogo: '/images/creative_it.jpeg',
+    providerName: 'Diane Croenwett',
+    createDate: 'Mar 19,2020',
+    tags: ['2hr, 47 min', '24 lessons'],
+  },
+  {
+    id: 4,
+    image: '/images/popular-course1.png',
+    title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
+    fee: '5,000',
+    providerLogo: '/images/creative_it.jpeg',
+    providerName: 'Diane Croenwett',
+    createDate: 'Mar 19,2020',
+    tags: ['2hr, 47 min', '24 lessons'],
+  },
+];
 const InstituteCourses = () => {
-  const {items} = usePagination({
-    count: 10,
-  });
-
-  const courseList = [
-    {
-      id: 1,
-      image: '/images/popular-course1.png',
-      title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
-      fee: '5,000',
-      providerLogo: '/images/creative_it.jpeg',
-      providerName: 'Diane Croenwett',
-      createDate: 'Mar 19,2020',
-      tags: ['2hr, 47 min', '24 lessons'],
-    },
-    {
-      id: 2,
-      image: '/images/popular-course1.png',
-      title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
-      fee: '5,000',
-      providerLogo: '/images/creative_it.jpeg',
-      providerName: 'Diane Croenwett',
-      createDate: 'Mar 19,2020',
-      tags: ['2hr, 47 min', '24 lessons'],
-    },
-    {
-      id: 3,
-      image: '/images/popular-course1.png',
-      title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
-      fee: '5,000',
-      providerLogo: '/images/creative_it.jpeg',
-      providerName: 'Diane Croenwett',
-      createDate: 'Mar 19,2020',
-      tags: ['2hr, 47 min', '24 lessons'],
-    },
-    {
-      id: 4,
-      image: '/images/popular-course1.png',
-      title: 'Design a Beautiful Stationary Set in Adobe Photoshop',
-      fee: '5,000',
-      providerLogo: '/images/creative_it.jpeg',
-      providerName: 'Diane Croenwett',
-      createDate: 'Mar 19,2020',
-      tags: ['2hr, 47 min', '24 lessons'],
-    },
-  ];
-
   const {messages} = useIntl();
   const [itemData, setItemData] = useState<any>('');
-  const classes = useStyles();
 
   const validationSchema = useMemo(() => {
     return yup.object().shape({
@@ -137,20 +103,20 @@ const InstituteCourses = () => {
         </Grid>
       </Grid>
       <Container maxWidth='xl'>
-        <Grid>
-          <Grid mt={12}>
-            <Grid md={12} container className={classes.filterMainGrid}>
-              <Grid item md={1}>
+        <Grid container mt={4} justifyContent={'center'}>
+          <Grid item md={12}>
+            <Grid container spacing={{xs: 2, md: 6}}>
+              <Grid item xs={12} md={1}>
                 <Grid container>
-                  <Grid item md={2}>
+                  <Grid item md={6}>
                     <FilterListIcon />
                   </Grid>
-                  <Grid ml={2} item md={2}>
-                    <Typography ml={2}>ফিল্টার</Typography>
+                  <Grid item md={6}>
+                    <Typography>ফিল্টার</Typography>
                   </Grid>
                 </Grid>
               </Grid>
-              <Grid item md={3}>
+              <Grid item xs={12} md={3}>
                 <CustomFormSelect
                   id='video_category'
                   label='কোর্সের নাম নির্বাচন করুন'
@@ -159,21 +125,21 @@ const InstituteCourses = () => {
                   optionValueProp={'id'}
                 />
               </Grid>
-              <Grid item md={3}>
+              <Grid item xs={12} md={3}>
                 <CustomFormSelect
                   id='select_video'
-                  label='প্রোগ্রাম নির্বাচন করুনf'
+                  label='প্রোগ্রাম নির্বাচন করুন'
                   isLoading={false}
                   control={control}
                   optionValueProp={'id'}
                 />
               </Grid>
-              <Grid item md={1}>
+              <Grid item xs={12} md={1}>
                 <Button variant={'contained'} color={'primary'}>
                   Reset
                 </Button>
               </Grid>
-              <Grid item md={3}>
+              <Grid item xs={12} md={4}>
                 <Paper
                   component='form'
                   sx={{
@@ -197,21 +163,11 @@ const InstituteCourses = () => {
               </Grid>
             </Grid>
           </Grid>
-          <Grid mt={8}>
-            <Grid container md={12} className={classes.cardMainGrid}>
+          <Grid item md={12} mt={{xs: 4, md: 5}}>
+            <Grid container>
               <Grid item xs={12}>
-                <Typography
-                  gutterBottom
-                  variant='h6'
-                  component='div'
-                  display={'flex'}>
-                  মোট ফলাফল পাওয়া গেছে
-                  <Typography
-                    variant='h6'
-                    component='div'
-                    className={classes.typographyNumber}>
-                    ১৫
-                  </Typography>
+                <Typography gutterBottom variant='h6'>
+                  মোট ফলাফল পাওয়া গেছে <Chip label={'15'} color={'primary'} />
                 </Typography>
               </Grid>
               <Grid item xs={12}>
@@ -232,36 +188,9 @@ const InstituteCourses = () => {
               </Grid>
             </Grid>
           </Grid>
-          <Grid mt={5} display={'flex'} justifyContent={'center'}>
-            <nav>
-              <List>
-                {items.map(({page, type, selected, ...item}, index) => {
-                  let children;
-                  if (type === 'start-ellipsis' || type === 'end-ellipsis') {
-                    children = '…';
-                  } else if (type === 'page') {
-                    children = (
-                      <button
-                        type='button'
-                        style={{
-                          fontWeight: selected ? 'bold' : undefined,
-                          padding: '10px',
-                        }}
-                        {...item}>
-                        {page}
-                      </button>
-                    );
-                  } else {
-                    children = (
-                      <button style={{padding: '10px'}} type='button' {...item}>
-                        {type}
-                      </button>
-                    );
-                  }
-                  return <li key={index}>{children}</li>;
-                })}
-              </List>
-            </nav>
+
+          <Grid item md={12} mt={4} display={'flex'} justifyContent={'center'}>
+            <Pagination count={4} variant='outlined' color='primary' />
           </Grid>
         </Grid>
       </Container>
