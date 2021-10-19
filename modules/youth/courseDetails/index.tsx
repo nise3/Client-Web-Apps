7305e975a5dcbfba43d9d0f129baa8363522646d
Animesh Karmakar <animesh.pust@gmail.com>
2021-10-19 11:46:39 +0600

    course details value set up.

--- modules/youth/courseDetails/index.tsx
+++ modules/youth/courseDetails/index.tsx
@@ -12,7 +12,6 @@ const CourseDetails = () => {
   let {courseId} = router.query;
 
   const {data: courseDetails} = useFetchCourseDetails(1);
-  console.log('course details', courseDetails);
 
   return (
     <Container maxWidth={'xl'} sx={{marginTop: 5, marginBottom: 5}}>
