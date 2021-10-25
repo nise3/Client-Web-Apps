4b268925ea172ac30882a2ebf32e9561039bd36e
Milton Deb <miltondeb.cse.bgc@gmail.com>
2021-10-25 19:32:01 +0600

    youth profile my cv link added

--- modules/youth/profile/MyCVSection.tsx
+++ modules/youth/profile/MyCVSection.tsx
@@ -1,9 +1,10 @@
-import {Box, Button, Card, CardContent, Grid, Typography} from '@mui/material';
-import {Add} from '@mui/icons-material';
+import {Button, Card, CardContent, Grid, Typography} from '@mui/material';
 import Image from 'next/image';
 import youthCV from '../../../public/images/youth/youth-cv.jpg';
 import React from 'react';
 import {useIntl} from 'react-intl';
+import {Link} from '../../../@softbd/elements/common';
+import {LINK_FRONTEND_YOUTH_MY_CV} from '../../../@softbd/common/appLinks';
 
 const MyCVSection = () => {
   const {messages} = useIntl();
@@ -11,55 +12,22 @@ const MyCVSection = () => {
   return (
     <Card>
       <CardContent>
-        <Grid container>
-          <Grid item xs={9}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
             <Typography variant={'h6'}>
               {messages['youth_profile.my_cv']}
             </Typography>
           </Grid>
-          <Grid item xs={3}>
-            <input
-              type='file'
-              accept='image/pdf/doc/*'
-              style={{display: 'none'}}
-              id='contained-button-file'
-            />
-            <label htmlFor='contained-button-file'>
-              <Button variant='contained' color='primary' component='span'>
-                <Add />
-              </Button>
-            </label>
-          </Grid>
           <Grid item xs={12}>
-            <Grid container>
-              <Grid item xs={8}>
-                <Image src={youthCV} />
-              </Grid>
-            </Grid>
+            <Image src={youthCV} />
           </Grid>
-
-          <Box mt={2} width={'100%'}>
-            <Grid item xs={12}>
-              <Grid container spacing={3}>
-                <Grid item xs={6}>
-                  <Button
-                    variant={'contained'}
-                    color={'primary'}
-                    fullWidth={true}>
-                    {messages['common.view']}
-                  </Button>
-                </Grid>
-                <Grid item xs={6}>
-                  <Button
-                    variant={'outlined'}
-                    color={'primary'}
-                    fullWidth={true}>
-                    {messages['common.download']}
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Box>
+          <Grid item xs={12} sx={{textAlign: 'center'}}>
+            <Link href={LINK_FRONTEND_YOUTH_MY_CV}>
+              <Button variant={'contained'} color={'primary'} fullWidth={true}>
+                {messages['common.view']}
+              </Button>
+            </Link>
+          </Grid>
         </Grid>
       </CardContent>
     </Card>
