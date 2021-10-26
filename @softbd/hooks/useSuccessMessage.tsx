05bd5ffa9125766345500010ab00e43128e22098
Hasan <hasan.softbdltd@gmail.com>
2021-10-26 10:55:35 +0600

    fix server side error processing.

---
+++ @softbd/hooks/useSuccessMessage.tsx
@@ -0,0 +1,30 @@
+import IntlMessages from '../../@crema/utility/IntlMessages';
+import React from 'react';
+import useNotiStack from './useNotifyStack';
+
+export default function useSuccessMessage() {
+  const {successStack} = useNotiStack();
+
+  const createSuccessMessage = (messageId: string) => {
+    successStack(
+      <IntlMessages
+        id='common.subject_created_successfully'
+        values={{subject: <IntlMessages id={messageId} />}}
+      />,
+    );
+  };
+
+  const updateSuccessMessage = (messageId: string) => {
+    successStack(
+      <IntlMessages
+        id='common.subject_updated_successfully'
+        values={{subject: <IntlMessages id={messageId} />}}
+      />,
+    );
+  };
+
+  return {
+    createSuccessMessage,
+    updateSuccessMessage,
+  };
+}
