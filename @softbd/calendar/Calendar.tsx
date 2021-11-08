0ea7a482ddc1c152df1dcf53662640365fb3477c
Hasan <hasan.softbdltd@gmail.com>
2021-11-08 19:46:38 +0600

    big calendar init.

--- @softbd/calendar/Calendar.tsx
+++ @softbd/calendar/Calendar.tsx
@@ -4,24 +4,34 @@ import {Calendar, momentLocalizer} from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
+const events = [
+  {
+    start: moment().toDate(),
+    end: moment().add(1, 'days').toDate(),
+    title: 'Some title',
+  },
+];
 
-const MyCalendar = () => (
-  <div>
-    <Calendar
-      localizer={localizer}
-      events={[
-        {
-          start: moment().toDate(),
-          end: moment().add(1, 'days').toDate(),
-          title: 'Some title',
-        },
-      ]}
-      // views={['month', 'day', 'agenda']}
-      startAccessor='start'
-      endAccessor='end'
-      style={{height: '100vh'}}
-    />
-  </div>
-);
+const MyCalendar = () => {
+  const handleSelect = ({start, end}: any) => {
+    const title = window.prompt('New Event name');
+    alert(title);
+  };
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{height: '100vh'}}
+        onSelectEvent={(event) => alert(event.title)}
+        selectable
+        onSelectSlot={handleSelect}
+      />
+    </div>
+  );
+};
 
 export default MyCalendar;
