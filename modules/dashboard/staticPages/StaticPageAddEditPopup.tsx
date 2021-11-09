2d72c4702a85c5a4f884185d9a40b95c98ffcdd8
Animesh Karmakar <animesh.pust@gmail.com>
2021-11-09 16:37:49 +0600

    validation added for show_in base dep of industry and organization

--- modules/dashboard/staticPages/StaticPageAddEditPopup.tsx
+++ modules/dashboard/staticPages/StaticPageAddEditPopup.tsx
@@ -2,7 +2,7 @@ import yup from '../../../@softbd/libs/yup';
 import Grid from '@mui/material/Grid';
 import {yupResolver} from '@hookform/resolvers/yup';
 import {SubmitHandler, useForm} from 'react-hook-form';
-import React, {FC, useEffect, useMemo, useState} from 'react';
+import React, {FC, useCallback, useEffect, useMemo, useState} from 'react';
 import HookFormMuiModal from '../../../@softbd/modals/HookFormMuiModal/HookFormMuiModal';
 import CustomTextInput from '../../../@softbd/elements/input/CustomTextInput/CustomTextInput';
 import CancelButton from '../../../@softbd/elements/button/CancelButton/CancelButton';
@@ -56,6 +56,7 @@ const StaticPageAddEditPopup: FC<StaticPageAddEditPopupProps> = ({
   const {createSuccessMessage, updateSuccessMessage} = useSuccessMessage();
   const isEdit = itemId != null;
   const authUser = useAuthUser();
+  const [selectedModule, setSelectedModule] = useState<number | null>(null);
 
   const {
     data: itemData,
@@ -78,10 +79,29 @@ const StaticPageAddEditPopup: FC<StaticPageAddEditPopupProps> = ({
         .title()
         .label(messages['common.title'] as string),
       sub_title: yup.string(),
-      institute_id: yup.string(),
-      organization_id: yup.string(),
+      institute_id: yup
+        .string()
+        .label(messages['common.institute'] as string)
+        .when('show_in', {
+          is: (val: number) => {
+            return val == 3;
+          },
+          then: yup.string().required(),
+        }),
+      organization_id: yup
+        .string()
+        .label(messages['common.organization_bn'] as string)
+        .when('show_in', {
+          is: (val: number) => {
+            return val == 4;
+          },
+          then: yup.string().required(),
+        }),
       organization_association_id: yup.string(),
-      content_type: yup.string().required().label('common.content_type'),
+      content_type: yup
+        .string()
+        .required()
+        .label(messages['common.content_type'] as string),
       contents: yup.string(),
       row_status: yup.string().trim().required(),
     });
@@ -118,19 +138,29 @@ const StaticPageAddEditPopup: FC<StaticPageAddEditPopupProps> = ({
     }
   }, [itemData]);
 
-  const modules = [
-    {id: 1, title: 'NISE3', title_en: 'NISE3'},
-    {id: 2, title: 'Youth', title_en: 'Youth'},
-    {id: 3, title: 'TSP', title_en: 'TSP'},
-    {id: 4, title: 'Industry', title_en: 'Industry'},
-    {id: 5, title: 'Industry Association', title_en: 'Industry Association'},
-  ];
+  const MODULES = useMemo(
+    () => [
+      {id: 1, title: 'NISE3', title_en: 'NISE3'},
+      {id: 2, title: 'Youth', title_en: 'Youth'},
+      {id: 3, title: 'TSP', title_en: 'TSP'},
+      {id: 4, title: 'Industry', title_en: 'Industry'},
+      {id: 5, title: 'Industry Association', title_en: 'Industry Association'},
+    ],
+    [],
+  );
 
-  const CONTENT_TYPES = [
-    {id: 1, title: 'Image', title_en: 'Image'},
-    {id: 2, title: 'Video', title_en: 'Video'},
-    {id: 3, title: 'Youtube', title_en: 'Youtube'},
-  ];
+  const CONTENT_TYPES = useMemo(
+    () => [
+      {id: 1, title: 'Image', title_en: 'Image'},
+      {id: 2, title: 'Video', title_en: 'Video'},
+      {id: 3, title: 'Youtube', title_en: 'Youtube'},
+    ],
+    [],
+  );
+
+  const onchangeModule = useCallback((moduleId: number | null) => {
+    setSelectedModule(moduleId);
+  }, []);
 
   const onSubmit: SubmitHandler<JobSector> = async (data: JobSector) => {
     try {
@@ -184,10 +214,11 @@ const StaticPageAddEditPopup: FC<StaticPageAddEditPopupProps> = ({
             label={messages['common.show_in']}
             control={control}
             isLoading={false}
-            options={modules}
+            options={MODULES}
             optionValueProp={'id'}
             optionTitleProp={['title_en', 'title']}
             errorInstance={errors}
+            onChange={onchangeModule}
           />
         </Grid>
         <Grid item xs={6}>
@@ -221,7 +252,7 @@ const StaticPageAddEditPopup: FC<StaticPageAddEditPopupProps> = ({
           />
         </Grid>
 
-        {authUser && authUser.isSystemUser && (
+        {authUser && authUser.isSystemUser && selectedModule == 4 && (
           <Grid item xs={6}>
             <CustomFormSelect
               id='organization_id'
@@ -236,7 +267,7 @@ const StaticPageAddEditPopup: FC<StaticPageAddEditPopupProps> = ({
           </Grid>
         )}
 
-        {authUser && authUser.isSystemUser && (
+        {authUser && authUser.isSystemUser && selectedModule == 3 && (
           <Grid item xs={6}>
             <CustomFormSelect
               id='institute_id'
