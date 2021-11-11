62d9b6207291e136867a45a8c8d29331853deb02
AfrarJahin <afrarjahin@gmail.com>
2021-11-11 18:24:52 +0600

    refactored

--- @crema/core/ThemeSetting/CustomColorCell.tsx
+++ @crema/core/ThemeSetting/CustomColorCell.tsx
@@ -1,11 +1,65 @@
 import React, {useContext} from 'react';
+import {styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import useStyles from './CustomColorCell.style';
 import {AppContext} from '../../index';
 import CheckIcon from '@mui/icons-material/Check';
 import IntlMessages from '../../utility/IntlMessages';
 import {ThemeMode} from '../../../shared/constants/AppEnums';
 import AppContextPropsType from '../../../redux/types/AppContextPropsType';
+
+const PREFIX = 'CustomColorCell';
+
+const classes = {
+  colorOption: `${PREFIX}-colorOption`,
+  colorOptionTriangle: `${PREFIX}-colorOptionTriangle`,
+  colorOptionBorder: `${PREFIX}-colorOptionBorder`,
+  colorOptionRightIcon: `${PREFIX}-colorOptionRightIcon`,
+  textBase: `${PREFIX}-textBase`,
+};
+
+const StyledBox = styled(Box)(({theme}) => ({
+  [`& .${classes.colorOption}`]: {
+    position: 'relative',
+    overflow: 'hidden',
+    cursor: 'pointer',
+    border: `1px solid ${theme.palette.text.primary}`,
+  },
+
+  [`& .${classes.colorOptionTriangle}`]: {
+    transform: 'rotate(45deg)',
+    marginTop: 10,
+    marginLeft: 22,
+  },
+
+  [`& .${classes.colorOptionBorder}`]: {
+    width: 10,
+    height: '100%',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    zIndex: 2,
+  },
+
+  [`& .${classes.colorOptionRightIcon}`]: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 20,
+    height: 20,
+    borderRadius: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    backgroundColor: '#fff',
+    color: theme.palette.primary.main,
+  },
+
+  [`& .${classes.textBase}`]: {
+    fontSize: 16,
+  },
+}));
 
 interface CustomColorCellProps {
   themeColorSet: any;
@@ -16,10 +70,9 @@ const CustomColorCell: React.FC<CustomColorCellProps> = ({
   themeColorSet,
   updateThemeColors,
 }) => {
-  const classes = useStyles();
   const {themeMode, theme} = useContext<AppContextPropsType>(AppContext);
   return (
-    <Box
+    <StyledBox
       component='li'
       onClick={() => {
         updateThemeColors(themeColorSet);
@@ -58,7 +111,7 @@ const CustomColorCell: React.FC<CustomColorCellProps> = ({
           </span>
         ) : null}
       </Box>
-    </Box>
+    </StyledBox>
   );
 };
 
