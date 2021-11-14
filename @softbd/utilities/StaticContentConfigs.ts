fd5333b232ed40c14c89bed750387f53e3f27176
Milton Deb <miltondeb.cse.bgc@gmail.com>
2021-11-14 15:29:39 +0600

    static page content id create

---
+++ @softbd/utilities/StaticContentConfigs.ts
@@ -0,0 +1,8 @@
+export const SELF_ASSESSMENT_CONTENT_ID = 'self_assessment';
+export const HOW_NISE3_WORKS_CONTENT_ID = 'how_nise3_works';
+export const ABOUT_US_CONTENT_ID = 'about_us';
+export const TERMS_AND_CONDITIONS_CONTENT_ID = 'terms_and_conditions';
+export const PRIVACY_POLICY_CONTENT_ID = 'privacy_policy';
+export const CAREER_ADVICE_CONTENT_ID = 'career_advice';
+export const FAQ_CONTENT_ID = 'faq';
+export const GUIDELINE_CONTENT_ID = 'guideline';
