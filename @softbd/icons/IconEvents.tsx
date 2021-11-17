b3149b86c14f8bc8f11b6a616aa62d8ce2b105c7
ehsan munna <grmunnabd@gmail.com>
2021-11-17 12:47:33 +0600

    refactor running

--- @softbd/icons/IconEvents.tsx
+++ @softbd/icons/IconEvents.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import {
-  EmojiEvents,
-  EmojiEventsOutlined,
-  EmojiEventsRounded,
-  EmojiEventsSharp,
-  EmojiEventsTwoTone,
+  EmojiEventsTwoTone
 } from '@mui/icons-material';
 
 const IconEvents = () => {
