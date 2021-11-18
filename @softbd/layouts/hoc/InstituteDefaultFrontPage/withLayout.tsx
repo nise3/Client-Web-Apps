0f9087bd4dc0673fe76f247c7df977a85ce4630f
naeemur.softbd <Window controlAboutnaeemur.rahman@softbdltd.com>
2021-11-18 13:07:24 +0600

    Restructure theming commit 2

--- @softbd/layouts/hoc/InstituteDefaultFrontPage/withLayout.tsx
+++ @softbd/layouts/hoc/InstituteDefaultFrontPage/withLayout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import InstituteDefaultLayout from '../../frontEnd/InstituteDefaultLayout';
-import {responsiveFontSizes} from '@mui/material';
+import {responsiveFontSizes, useTheme} from '@mui/material';
 import {ThemeProvider} from '@mui/material/styles';
 import theme from '../../themes/instituteDefault';
 
 const withLayout = (ComposedComponent: any) => (props: any) => {
+  const defaultTheme = useTheme();
   return (
-    <ThemeProvider theme={responsiveFontSizes(theme())}>
+    <ThemeProvider theme={responsiveFontSizes(theme(defaultTheme))}>
       <InstituteDefaultLayout>
         <ComposedComponent {...props} />
       </InstituteDefaultLayout>
