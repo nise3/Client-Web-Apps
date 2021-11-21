2e064a134d616b77e6dc23ff094dc6a0fa6bbf44
Hasan <hasan.softbdltd@gmail.com>
2021-11-21 17:59:27 +0600

    fixing dashboard design issue.

--- @softbd/layouts/themes/dashboard/componentStyleOverrides.tsx
+++ @softbd/layouts/themes/dashboard/componentStyleOverrides.tsx
@@ -1,5 +1,8 @@
 import {Theme} from '@mui/system';
 import {Components} from '@mui/material/styles/components';
+import {ThemeStyleRadius} from '../../../../shared/constants/AppEnums';
+
+const cardRadius = ThemeStyleRadius.STANDARD;
 
 export default function componentStyleOverrides(theme: Theme) {
   const overrideRules: Components = {
@@ -21,6 +24,36 @@ export default function componentStyleOverrides(theme: Theme) {
         },
         label: {
           fontSize: '1rem',
+        },
+      },
+    },
+    MuiToggleButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: cardRadius,
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: cardRadius,
+          boxShadow: '0px 5px 6px rgba(0, 0, 0, 0.04)',
+          '& .MuiCardContent-root:last-child': {
+            paddingBottom: 16,
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: cardRadius,
+          boxShadow: '0px 5px 6px rgba(0, 0, 0, 0.04)',
+          [theme.breakpoints.down('md')]: {
+            paddingTop: '8px !important',
+            paddingBottom: '8px !important',
+          },
         },
       },
     },
