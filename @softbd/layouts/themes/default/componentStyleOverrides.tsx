0fdc3556c9053a20b5b49aca2ed8f78a97d464c0
Hasan <hasan.softbdltd@gmail.com>
2021-11-21 11:55:59 +0600

    fixing theme issue.

--- @softbd/layouts/themes/default/componentStyleOverrides.tsx
+++ @softbd/layouts/themes/default/componentStyleOverrides.tsx
@@ -1,17 +1,28 @@
 import {Theme} from '@mui/system';
 import {Components} from '@mui/material/styles/components';
+import {ThemeMode} from '../../../../shared/constants/AppEnums';
 
 export default function componentStyleOverrides(theme: Theme, locale: string) {
   const overrideRules: Components = {
-    MuiButton: {},
     MuiFormLabel: {
-      styleOverrides: {},
+      styleOverrides: {
+        asterisk: {
+          color: '#db3131',
+          '&$error': {
+            color: '#db3131',
+          },
+        },
+      },
     },
-    MuiTextField: {
-      styleOverrides: {},
-    },
-    MuiTypography: {
-      styleOverrides: {},
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          background:
+            theme.palette.mode === ThemeMode.LIGHT
+              ? theme.palette.grey['50']
+              : theme.palette.grey['900'],
+        },
+      },
     },
   };
 
