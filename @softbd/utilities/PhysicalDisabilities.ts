e7d6392e093a5fb83cde22022dc675f58f0f12e3
Milton Deb <miltondeb.cse.bgc@gmail.com>
2021-11-22 15:18:02 +0600

    physical disability data type fix

--- @softbd/utilities/PhysicalDisabilities.ts
+++ @softbd/utilities/PhysicalDisabilities.ts
@@ -1,9 +1,9 @@
 enum PhysicalDisabilities {
-  VISUAL = '1',
-  HEARING = '2',
-  MENTAL_HEALTH = '3',
-  INTELLECTUAL = '4',
-  SOCIAL = '5',
+  VISUAL = 1,
+  HEARING = 2,
+  MENTAL_HEALTH = 3,
+  INTELLECTUAL = 4,
+  SOCIAL = 5,
 }
 
 export default PhysicalDisabilities;
