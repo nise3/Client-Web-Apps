11d09543fb00375c653314ee057cac42a8d4aa4f
Hasan <hasan.softbdltd@gmail.com>
2021-12-02 20:24:02 +0600

    auth redirection fixed.

--- @softbd/common/constants.ts
+++ @softbd/common/constants.ts
@@ -3,3 +3,21 @@ export const APP_TITLE = 'NISE3 TMS';
 export const PERMISSION_GROUP_ORGANIZATION_KEY = 'organization';
 export const PERMISSION_GROUP_INSTITUTE_KEY = 'institute';
 export const HIERARCHY_NODE_ID_PREFIX_STRING = 'm';
+
+const isLocalHost = () => location.hostname === "localhost" || location.hostname === "127.0.0.1";
+
+export const dashboardDomain = () => {
+    return isLocalHost() ? 'http://localhost:3000' : 'http://123.49.47.38:3000'
+}
+
+export const niseDomain = () => {
+    return isLocalHost() ? 'http://localhost:3001' : 'http://123.49.47.38:3001'
+}
+
+export const youthDomain = () => {
+    return isLocalHost() ? 'http://localhost:3002' : 'http://123.49.47.38:3002'
+}
+
+export const instituteDomain = () => {
+    return isLocalHost() ? 'http://localhost:3003' : 'http://123.49.47.38:3003'
+}
--- @softbd/layouts/hoc/DashboardPage/withData.tsx
+++ @softbd/layouts/hoc/DashboardPage/withData.tsx
@@ -3,22 +3,26 @@ import {useSelector} from 'react-redux';
 import Loader from '../../../../@crema/core/Loader';
 import {AppState} from '../../../../redux/store';
 import {getSSOLoginUrl} from '../../../common/SSOConfig';
+import {youthDomain} from "../../../common/constants";
 
 const withData = (ComposedComponent: any) => (props: any) => {
-  const {user, loading} = useSelector<AppState, AppState['auth']>(
-    ({auth}) => auth,
-  );
-  // const {asPath} = useRouter();
-  // const queryParams = asPath.split('?')[1];
-  useEffect(() => {
-    if (!user && !loading) {
-      // Router.push('/signin' + (queryParams ? '?' + queryParams : ''));
-      window.location.href = getSSOLoginUrl();
-    }
-  }, [user, loading]);
-  if (!user || loading) return <Loader />;
+    const {user, loading} = useSelector<AppState, AppState['auth']>(
+        ({auth}) => auth,
+    );
+    // const {asPath} = useRouter();
+    // const queryParams = asPath.split('?')[1];
+    useEffect(() => {
+        if (!user && !loading) {
+            // Router.push('/signin' + (queryParams ? '?' + queryParams : ''));
+            window.location.href = getSSOLoginUrl();
+        }
+        if (user && user.isYouthUser) {
+            window.location.href = youthDomain();
+        }
+    }, [user, loading]);
+    if (!user || loading) return <Loader/>;
 
-  return <ComposedComponent {...props} />;
+    return <ComposedComponent {...props} />;
 };
 
 export default withData;
