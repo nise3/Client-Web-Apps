76e4f9dcbe1d45f37b24491002132787c2d92c88
Milton Deb <miltondeb.cse.bgc@gmail.com>
2021-12-05 14:51:47 +0600

    dashboard and registration page issue fix

--- modules/events/EventMiniCalendarView.tsx
+++ modules/events/EventMiniCalendarView.tsx
@@ -6,17 +6,19 @@ import {useFetchCalenderEvents} from '../../services/cmsManagement/hooks';
 import {Box, Card, CardContent, Grid} from '@mui/material';
 import EventCalendarDetails from './EventCalendarDetails';
 import CancelButton from '../../@softbd/elements/button/CancelButton/CancelButton';
-import {ICalendar, ICalendarQuery} from '../../shared/Interface/common.interface';
-import {addStartEndPropsToList, eventsDateTimeMap} from '../../services/global/globalService';
+import {
+  ICalendar,
+  ICalendarQuery,
+} from '../../shared/Interface/common.interface';
+import {
+  addStartEndPropsToList,
+  eventsDateTimeMap,
+} from '../../services/global/globalService';
 import CustomFormSelect from '../../@softbd/elements/input/CustomFormSelect/CustomFormSelect';
 import {useForm} from 'react-hook-form';
 
 const localizer = momentLocalizer(moment);
 const EventMiniCalendarView = () => {
-  let requestQuery: ICalendarQuery = {
-    type: 'month',
-  };
-
   const {
     control,
     reset,
@@ -24,7 +26,9 @@ const EventMiniCalendarView = () => {
   } = useForm<any>();
 
   const [selectedItem, setSelectedItem] = useState<ICalendar>();
-  const [viewFilters, setViewFilters] = useState<ICalendarQuery>(requestQuery);
+  const [viewFilters, setViewFilters] = useState<ICalendarQuery>({
+    type: 'month',
+  });
   const [eventsList, setEventsList] = useState<Array<ICalendar>>([]);
 
   const [isOpenDetailsView, setIsOpenDetailsView] = useState(false);
@@ -37,19 +41,20 @@ const EventMiniCalendarView = () => {
 
   useEffect(() => {
     if (events) {
-      events = eventsDateTimeMap(events);
-      setEventsList(events);
+      setEventsList(eventsDateTimeMap(events));
     }
   }, [events]);
 
   useEffect(() => {
-      reset({
-        inst_id: '1'
-      });
+    reset({
+      inst_id: '1',
+    });
   }, []);
 
   const onSelectEvent = (e: any) => {
-    const item = eventsList.find((ev: ICalendar) => ev.id === e.id) as ICalendar;
+    const item = eventsList.find(
+      (ev: ICalendar) => ev.id === e.id,
+    ) as ICalendar;
     setSelectedItem(item);
     setIsOpenDetailsView(true);
     // console.log(item);
@@ -59,49 +64,51 @@ const EventMiniCalendarView = () => {
   };
 
   return (
-      <Card>
-        <Grid style={{padding: 20}} xs={6} md={6}>
-          <CustomFormSelect
-            id='inst_id'
-            // label={'Institute Calendar'}
-            isLoading={false}
-            control={control}
-            options={[{'id': 1, 'name': 'Institute Calendar'}]}
-            optionValueProp={'id'}
-            optionTitleProp={['name']}
-            errorInstance={errors}
-          />
-        </Grid>
-        <CardContent>
-          <Grid item xs={12} md={12}>
-            {isOpenDetailsView ? (
-              <div>
-                <EventCalendarDetails itemData={selectedItem} />
-                <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
-                  <Box style={{paddingTop: 20}}>
-                    <CancelButton onClick={onClose} isLoading={false} />
-                  </Box>
+    <Card>
+      <Grid style={{padding: 20}} xs={6} md={6}>
+        <CustomFormSelect
+          id='inst_id'
+          // label={'Institute Calendar'}
+          isLoading={false}
+          control={control}
+          options={[{id: 1, name: 'Institute Calendar'}]}
+          optionValueProp={'id'}
+          optionTitleProp={['name']}
+          errorInstance={errors}
+        />
+      </Grid>
+      <CardContent>
+        <Grid item xs={12} md={12}>
+          {isOpenDetailsView ? (
+            <div>
+              <EventCalendarDetails itemData={selectedItem} />
+              <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+                <Box style={{paddingTop: 20}}>
+                  <CancelButton onClick={onClose} isLoading={false} />
                 </Box>
-              </div>
-            ) : (
-              <Calendar
-                events={eventsList || null}
-                localizer={localizer}
-                selectable={true}
-                style={{height: 500}}
-                startAccessor='start'
-                endAccessor='end'
-                defaultDate={moment().toDate()}
-                views={['month']}
-                onView={(view: View) =>
-                  setViewFilters({...requestQuery, ...{type: view}})
-                }
-                onSelectEvent={onSelectEvent}
-              />
-            )}
-          </Grid>
-        </CardContent>
-      </Card>
+              </Box>
+            </div>
+          ) : (
+            <Calendar
+              events={eventsList || null}
+              localizer={localizer}
+              selectable={true}
+              style={{height: 500}}
+              startAccessor='start'
+              endAccessor='end'
+              defaultDate={moment().toDate()}
+              views={['month']}
+              onView={(view: View) =>
+                setViewFilters((prev) => {
+                  return {...prev, ...{type: view}};
+                })
+              }
+              onSelectEvent={onSelectEvent}
+            />
+          )}
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
