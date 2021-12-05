49073acda5af91f7b4bf4716fab40ac003c9c990
Hasan <hasan.softbdltd@gmail.com>
2021-12-05 11:13:54 +0600

    add bash.

---
+++ builder.sh
@@ -0,0 +1,23 @@
+yarn
+yarn build:nise
+pm2 stop nise
+pm2 delete nise
+pm2 start yarn --name "nise" --interpreter bash -- start:nise
+
+yarn
+yarn build:institute
+pm2 stop institute
+pm2 delete institute
+pm2 start yarn --name "institute" --interpreter bash -- start:institute
+
+yarn
+yarn build:admin
+pm2 stop admin
+pm2 delete admin
+pm2 start yarn --name "admin" --interpreter bash -- start:admin
+
+yarn
+yarn build:youth
+pm2 stop youth
+pm2 delete youth
+pm2 start yarn --name "youth" --interpreter bash -- start:youth
