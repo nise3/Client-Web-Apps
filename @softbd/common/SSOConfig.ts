a073731406614573f9cbbcb8b45e6a3dd1e23186
Hasan <hasan.softbdltd@gmail.com>
2021-12-06 17:21:27 +0600

    cookie issue fixed.

--- @softbd/common/SSOConfig.ts
+++ @softbd/common/SSOConfig.ts
@@ -1,33 +1,32 @@
+import {niseDomain} from "./constants";
+
 interface TConfig {
-  authUrl: string;
-  logoutUrl: string;
-  tokenUrl: string;
-  clientKey: string;
-  clientSecret: string;
-  callbackUrl: string;
+    authUrl: string;
+    logoutUrl: string;
+    tokenUrl: string;
+    clientKey: string;
+    clientSecret: string;
+    callbackUrl: string;
 }
 
 const SSO_CONFIG: TConfig = {
-  authUrl: 'https://bus-staging.softbdltd.com/oauth2/authorize/',
-  logoutUrl: 'https://bus-staging.softbdltd.com/oidc/logout',
-  tokenUrl: 'https://bus-staging.softbdltd.com/oauth2/token',
-  clientKey: 'FhVqwNp6Q6FV1H8KuuLsh5REQysa',
-  clientSecret: 'GfrDpy904LjaWNmn7aSwEA1qyEQa',
-  callbackUrl: '/callback',
+    authUrl: 'https://bus-staging.softbdltd.com/oauth2/authorize/',
+    logoutUrl: 'https://bus-staging.softbdltd.com/oidc/logout',
+    tokenUrl: 'https://bus-staging.softbdltd.com/oauth2/token',
+    clientKey: 'FhVqwNp6Q6FV1H8KuuLsh5REQysa',
+    clientSecret: 'GfrDpy904LjaWNmn7aSwEA1qyEQa',
+    callbackUrl: niseDomain(),
 };
 
-export const getSSOLoginUrl = () => {
-  const origin = getHostUrl();
-
-  return (
-    SSO_CONFIG.authUrl +
-    '?response_type=code&client_id=' +
-    SSO_CONFIG.clientKey +
-    '&scope=openid email&nonce=13e2312637dg136e1&' +
-    'redirect_uri=' +
-    origin +
-    SSO_CONFIG.callbackUrl
-  );
+export const getSSOLoginUrl = (callbackUrl?: string) => {
+    return (
+        SSO_CONFIG.authUrl +
+        '?response_type=code&client_id=' +
+        SSO_CONFIG.clientKey +
+        '&scope=openid email&nonce=13e2312637dg136e1&' +
+        'redirect_uri=' +
+        callbackUrl || SSO_CONFIG.callbackUrl
+    );
 };
 
 // export const getSSOLoginUrl = () => {
@@ -48,24 +47,24 @@ export const getSSOLoginUrl = () => {
 // };
 
 export const getSSOLogoutUrl = (idToken: string) => {
-  const origin = getHostUrl();
+    const origin = getHostUrl();
 
-  return (
-    SSO_CONFIG.logoutUrl +
-    '?id_token_hint=' +
-    idToken +
-    '&post_logout_redirect_uri=' +
-    origin +
-    '/logout' +
-    '&state=' +
-    'hello'
-  );
+    return (
+        SSO_CONFIG.logoutUrl +
+        '?id_token_hint=' +
+        idToken +
+        '&post_logout_redirect_uri=' +
+        origin +
+        '/logout' +
+        '&state=' +
+        'hello'
+    );
 };
 
 export const getHostUrl = () => {
-  return typeof window !== 'undefined' && window.location.origin
-    ? window.location.origin
-    : '';
+    return typeof window !== 'undefined' && window.location.origin
+        ? window.location.origin
+        : '';
 };
 
 export default SSO_CONFIG;
--- @softbd/layouts/frontEnd/InstituteDefaultLayout/Header.tsx
+++ @softbd/layouts/frontEnd/InstituteDefaultLayout/Header.tsx
@@ -7,7 +7,6 @@ import Menu from '@mui/material/Menu';
 import clsx from 'clsx';
 import Box from '@mui/material/Box';
 import {Login} from '@mui/icons-material';
-/*import LogoCustomizable from '../../../elements/common/LogoCustomizable';*/
 import {NavLink as Link, Text} from '../../../elements/common';
 import {
   LINK_FRONTEND_INSTITUTE_CALENDAR,
@@ -22,14 +21,15 @@ import {
 import {classes, StyledAppBar, StyledBox} from './Header.style';
 import {useIntl} from 'react-intl';
 import {Container, Grid} from '@mui/material';
-// import {getSSOLoginUrl} from '../../../common/SSOConfig';
 import LanguageSwitcher from '../../../../@crema/core/LanguageSwitcher';
 import GotoDashboardButton from '../../../elements/button/GotoDashboardButton/GotoDashboardButton';
 import {useAuthUser, useVendor} from '../../../../@crema/utility/AppHooks';
 import {CurrentInstitute} from '../../../../redux/types/models/Vendor';
 import {niseDomain} from '../../../common/constants';
+import {getHostUrl} from "../../../common/SSOConfig";
 
-interface AppHeaderProps {}
+interface AppHeaderProps {
+}
 
 const Header: React.FC<AppHeaderProps> = () => {
   const authUser = useAuthUser();
@@ -100,14 +100,14 @@ const Header: React.FC<AppHeaderProps> = () => {
         </Link>
       </MenuItem>
       <MenuItem component='span' className={classes.menuItemMobile}>
-        <LanguageSwitcher />
+        <LanguageSwitcher/>
       </MenuItem>
       <MenuItem component='span' className={classes.menuItemMobile}>
         {authUser ? (
-          <GotoDashboardButton />
+          <GotoDashboardButton/>
         ) : (
-          <Link href={niseDomain() + LINK_SIGNUP}>
-            <Login className={classes.menuIcons} />
+          <Link href={niseDomain() + LINK_SIGNUP + '?login_request_from=' + getHostUrl()}>
+            <Login className={classes.menuIcons}/>
             {messages['common.registration_login']}
           </Link>
         )}
@@ -230,15 +230,15 @@ const Header: React.FC<AppHeaderProps> = () => {
 
             <Box className={classes.headerMenuGroup}>
               <Box sx={{height: '100%'}} className={classes.languageSwitcher}>
-                <LanguageSwitcher />
+                <LanguageSwitcher/>
               </Box>
               {authUser ? (
-                <GotoDashboardButton />
+                <GotoDashboardButton/>
               ) : (
                 <Link
-                  href={niseDomain() + LINK_SIGNUP}
+                  href={niseDomain() + LINK_SIGNUP + '?login_request_from=' + getHostUrl()}
                   className={classes.menuItemRegOrLogin}>
-                  <Login className={classes.menuIcons} />
+                  <Login className={classes.menuIcons}/>
                   {messages['common.registration_login']}
                 </Link>
               )}
@@ -253,7 +253,7 @@ const Header: React.FC<AppHeaderProps> = () => {
                 // color='inherit'
                 className={classes.mobileMenuButton}
                 size='large'>
-                <MoreIcon />
+                <MoreIcon/>
               </IconButton>
             </Box>
           </Container>
