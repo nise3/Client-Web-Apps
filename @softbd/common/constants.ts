bd14905467799304c41d8962e222a10044408801
Hasan <hasan.softbdltd@gmail.com>
2021-12-06 17:41:00 +0600

    cookie issue fixed.

--- @softbd/common/constants.ts
+++ @softbd/common/constants.ts
@@ -4,7 +4,7 @@ export const PERMISSION_GROUP_ORGANIZATION_KEY = 'organization';
 export const PERMISSION_GROUP_INSTITUTE_KEY = 'institute';
 export const HIERARCHY_NODE_ID_PREFIX_STRING = 'm';
 
-const isLocalHost = () => window?.location?.hostname ? location.hostname === "localhost" || location.hostname === "127.0.0.1" : '';
+const isLocalHost = () => typeof window !== 'undefined' && window?.location?.hostname ? location.hostname === "localhost" || location.hostname === "127.0.0.1" : '';
 
 export const adminDomain = () => {
   return isLocalHost() ? 'http://localhost:3000' : 'https://admin.nise3.xyz'
