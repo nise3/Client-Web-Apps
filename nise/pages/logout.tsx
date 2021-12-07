91dabc6436e4d879676fc12cf69f7e3bf237e9f8
Hasan <hasan.softbdltd@gmail.com>
2021-12-07 15:18:12 +0600

    solve app access token issue.

--- nise/pages/logout.tsx
+++ nise/pages/logout.tsx
@@ -6,6 +6,8 @@ import {useRouter} from 'next/router';
 import {niseDomain} from "../../@softbd/common/constants";
 import {CommonAuthUser} from "../../redux/types/models/CommonAuthUser";
 import NiseFrontPage from "../../@softbd/layouts/hoc/NiseFrontPage";
+import cookieInstance from "../../@softbd/libs/cookieInstance";
+import {COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA, COOKIE_KEY_AUTH_ID_TOKEN} from "../../shared/constants/AppConst";
 
 export default NiseFrontPage(() => {
   const dispatch = useDispatch();
@@ -16,10 +18,14 @@ export default NiseFrontPage(() => {
   useEffect(() => {
     if (authUser) {
       dispatch(onJWTAuthSignout());
+      cookieInstance.remove(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
+      cookieInstance.remove(COOKIE_KEY_AUTH_ID_TOKEN);
       setStaleAuthUser(authUser);
     } else {
       if (staleAuthUser?.institute?.domain) {
-        router.push(staleAuthUser?.institute?.domain);
+        const logoutUrl = new URL(staleAuthUser?.institute?.domain);
+        logoutUrl.pathname = '/logout';
+        router.push(logoutUrl.toString());
       } else {
         router.push(niseDomain());
       }
