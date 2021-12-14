026683d35d3c92e4cffb9e2ad3db634c270a376a
shakib77 <shakibshaon77@gmail.com>
2021-12-14 18:12:58 +0600

    Association Profile 1. view page is done 2. working on edit page

---
+++ dashboard/pages/profile/index.tsx
@@ -0,0 +1,18 @@
+import asyncComponent from '../../../@crema/utility/asyncComponent';
+import PageMeta from '../../../@crema/core/PageMeta';
+import DashboardPage from '../../../@softbd/layouts/hoc/DashboardPage';
+import React from 'react';
+import {useIntl} from 'react-intl';
+
+const Profile = asyncComponent(
+  () => import('../../../modules/dashboard/profile/index'),
+);
+export default DashboardPage(() => {
+  const {messages} = useIntl();
+  return (
+    <>
+      <PageMeta title={messages['common.profile']} />
+      <Profile />
+    </>
+  );
+});
