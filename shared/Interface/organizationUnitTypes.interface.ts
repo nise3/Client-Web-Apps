b93ddfdaa947afc4dfdf3faaddc8d8b0920e2b0f
Asir053 <asirbillah@gmail.com>
2021-12-29 11:15:06 +0600

    Removed unnecessary interfaces and fixed optional and mandatory properties

--- shared/Interface/organizationUnitTypes.interface.ts
+++ shared/Interface/organizationUnitTypes.interface.ts
@@ -1,8 +1,6 @@
-import {IidTitleCreateUpdateAt, IRowStatus} from './common.interface';
+import {IIdTitleCreateUpdateAt} from './common.interface';
 
-export interface IOrganizationUnitType
-  extends IidTitleCreateUpdateAt,
-    IRowStatus {
+export interface IOrganizationUnitType extends IIdTitleCreateUpdateAt {
   organization_id: number;
-  //row_status?: number | string;
+  row_status?: number | string;
 }
