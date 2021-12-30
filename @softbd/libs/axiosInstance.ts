1260cad9ec6fd943f31378359a6deff8b570a9f4
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2021-12-30 18:20:47 +0600

    local sso cofiguration on going

--- @softbd/libs/axiosInstance.ts
+++ @softbd/libs/axiosInstance.ts
@@ -1,152 +1,158 @@
 import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
 import {API_BASE_URL} from '../common/apiRoutes';
 import {
-  COOKIE_KEY_APP_ACCESS_TOKEN,
-  COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA,
+    COOKIE_KEY_APP_ACCESS_TOKEN,
+    COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA, IS_LOCAL_IDP,
 } from '../../shared/constants/AppConst';
 import {
-  getBrowserCookie,
-  removeBrowserCookie,
-  setBrowserCookie,
+    getBrowserCookie,
+    removeBrowserCookie,
+    setBrowserCookie,
 } from './cookieInstance';
 import registerAxiosMockAdapter from './registerAxiosMockAdapter';
 import {getSSOLoginUrl} from '../common/SSOConfig';
 
 let retryAuthRefreshToken = 0;
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: API_BASE_URL,
-  timeout: 300000,
+    baseURL: API_BASE_URL,
+    timeout: 300000,
 });
 axiosInstance.defaults.headers.common['Accept'] = 'application/json';
 axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
 
 axiosInstance.interceptors.request.use(
-  async (config: AxiosRequestConfig) => {
-    const authAccessTokenData = getBrowserCookie(
-      COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA,
-    );
-    // console.log('authAccessTokenData', authAccessTokenData);
-    const userAccessToken = authAccessTokenData?.access_token;
-
-    //TODO: temporary
-    if (!config.headers['Authorization']) {
-      if (userAccessToken) {
-        config.headers['Authorization'] = `Bearer ${userAccessToken}`;
-      } else {
-        const appAccessTokenData = getBrowserCookie(
-          COOKIE_KEY_APP_ACCESS_TOKEN,
+    async (config: AxiosRequestConfig) => {
+        const authAccessTokenData = getBrowserCookie(
+            COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA,
         );
-        config.headers[
-          'Authorization'
-        ] = `Bearer ${appAccessTokenData?.access_token}`;
-      }
-    }
+        // console.log('authAccessTokenData', authAccessTokenData);
+        const userAccessToken = authAccessTokenData?.access_token;
 
-    return config;
-  },
-  (error) => {
-    Promise.reject(error);
-  },
+        //TODO: temporary
+        if (!config.headers['Authorization']) {
+            if (userAccessToken) {
+                config.headers['Authorization'] = `Bearer ${userAccessToken}`;
+            } else {
+                const appAccessTokenData = getBrowserCookie(
+                    COOKIE_KEY_APP_ACCESS_TOKEN,
+                );
+                config.headers[
+                    'Authorization'
+                    ] = `Bearer ${appAccessTokenData?.access_token}`;
+            }
+        }
+
+        return config;
+    },
+    (error) => {
+        Promise.reject(error);
+    },
 );
 
 // Response interceptor for API calls
 axiosInstance.interceptors.response.use(
-  (response) => {
-    console.count('countSuccessRequest');
-    return response;
-  },
-  async function (error) {
-    console.count('countFailedRequest');
-    if (error?.response?.status === 401) {
-      const authAccessTokenData = getBrowserCookie(
-        COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA,
-      );
-      if (authAccessTokenData) {
-        await refreshAuthAccessToken();
-      } else {
-        await refreshAppAccessToken();
-      }
-    }
-    return Promise.reject(error);
-  },
+    (response) => {
+        console.count('countSuccessRequest');
+        return response;
+    },
+    async function (error) {
+        console.count('countFailedRequest');
+        if (error?.response?.status === 401) {
+            const authAccessTokenData = getBrowserCookie(
+                COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA,
+            );
+            if (authAccessTokenData) {
+                await refreshAuthAccessToken();
+            } else {
+                await refreshAppAccessToken();
+            }
+        }
+        return Promise.reject(error);
+    },
 );
 
 export function setDefaultAuthorizationHeader(accessToken?: string) {
-  // console.log('setDefaultAuthorizationHeader', accessToken);
-  axiosInstance.defaults.headers.common['Authorization'] =
-    'Bearer ' + accessToken || '';
+    // console.log('setDefaultAuthorizationHeader', accessToken);
+    axiosInstance.defaults.headers.common['Authorization'] =
+        'Bearer ' + accessToken || '';
 }
 
 async function refreshAuthAccessToken() {
-  if (retryAuthRefreshToken === 20) {
-    removeBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
-    window.location.href = getSSOLoginUrl();
-  }
-  retryAuthRefreshToken++;
-  console.log('refreshAuthAccessToken');
-  const authAccessTokenData = getBrowserCookie(
-    COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA,
-  );
-
-  var appAccessTokenData = getBrowserCookie(COOKIE_KEY_APP_ACCESS_TOKEN);
-  if (!appAccessTokenData) {
-    await refreshAppAccessToken();
-    appAccessTokenData = getBrowserCookie(COOKIE_KEY_APP_ACCESS_TOKEN);
-  }
-
-  if (authAccessTokenData?.refresh_token) {
-    try {
-      let {
-        data: {id_token, ...responseTokenData},
-      } = await axiosInstance.post(
-        'https://core.bus-staging.softbdltd.com/sso-renew-access-token',
-        {
-          refresh_token: authAccessTokenData.refresh_token,
-        },
-        {
-          headers: {
-            Authorization: appAccessTokenData?.access_token,
-          },
-        },
-      );
-
-      setBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA, responseTokenData);
-
-      //TODO: temporary
-      setDefaultAuthorizationHeader(responseTokenData.access_token);
-      retryAuthRefreshToken = 0;
-    } catch (e) {
-      console.log('refreshAuthAccessToken-error', e);
-      removeBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
+    if (retryAuthRefreshToken === 20) {
+        removeBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
+        window.location.href = getSSOLoginUrl();
     }
-  } else {
-    removeBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
-  }
+    retryAuthRefreshToken++;
+    console.log('refreshAuthAccessToken');
+    const authAccessTokenData = getBrowserCookie(
+        COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA,
+    );
+
+    var appAccessTokenData = getBrowserCookie(COOKIE_KEY_APP_ACCESS_TOKEN);
+    if (!appAccessTokenData) {
+        await refreshAppAccessToken();
+        appAccessTokenData = getBrowserCookie(COOKIE_KEY_APP_ACCESS_TOKEN);
+    }
+
+    if (authAccessTokenData?.refresh_token) {
+
+        let urlHost = 'https://core.bus-staging.softbdltd.com';
+        if (IS_LOCAL_IDP) {
+            urlHost = 'http://nise-core.softbd'
+        }
+
+        try {
+            let {
+                data: {id_token, ...responseTokenData},
+            } = await axiosInstance.post(
+                urlHost + '/sso-renew-access-token',
+                {
+                    refresh_token: authAccessTokenData.refresh_token,
+                },
+                {
+                    headers: {
+                        Authorization: appAccessTokenData?.access_token,
+                    },
+                },
+            );
+
+            setBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA, responseTokenData);
+
+            //TODO: temporary
+            setDefaultAuthorizationHeader(responseTokenData.access_token);
+            retryAuthRefreshToken = 0;
+        } catch (e) {
+            console.log('refreshAuthAccessToken-error', e);
+            removeBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
+        }
+    } else {
+        removeBrowserCookie(COOKIE_KEY_AUTH_ACCESS_TOKEN_DATA);
+    }
 }
 
 export async function refreshAppAccessToken() {
-  try {
-    let response = await getAppAccessToken({
-      throwError: true,
-    });
-    setBrowserCookie(COOKIE_KEY_APP_ACCESS_TOKEN, response?.data);
-    //TODO: temporary
-    setDefaultAuthorizationHeader(response?.data?.access_token);
-  } catch (e) {
-    console.log(e);
-  }
+    try {
+        let response = await getAppAccessToken({
+            throwError: true,
+        });
+        setBrowserCookie(COOKIE_KEY_APP_ACCESS_TOKEN, response?.data);
+        //TODO: temporary
+        setDefaultAuthorizationHeader(response?.data?.access_token);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 export async function getAppAccessToken({throwError = false} = {}) {
-  try {
-    return await axios.get(
-      'https://core.bus-staging.softbdltd.com/nise3-app-api-access-token',
-    );
-  } catch (e: any) {
-    if (throwError) {
-      throw e;
+    try {
+        return await axios.get(
+            'https://core.bus-staging.softbdltd.com/nise3-app-api-access-token',
+        );
+    } catch (e: any) {
+        if (throwError) {
+            throw e;
+        }
     }
-  }
 }
 
 registerAxiosMockAdapter(axiosInstance);
