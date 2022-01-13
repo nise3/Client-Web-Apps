18262506ac6964a473e271da62ec8ca620744040
shakib77 <shakibshaon77@gmail.com>
2022-01-13 18:05:16 +0600

    feat: industry association contact info implemented in admin and landing

---
+++ modules/dashboard/industryAssociationContactInfo/IndustryAssociationContactInfoDetailsPopup.tsx
@@ -0,0 +1,95 @@
+import {useFetchContactInfo} from '../../../services/IndustryAssociationManagement/hooks';
+import {useIntl} from 'react-intl';
+import CustomDetailsViewMuiModal from '../../../@softbd/modals/CustomDetailsViewMuiModal/CustomDetailsViewMuiModal';
+import {BusinessCenter} from '@mui/icons-material';
+import IntlMessages from '../../../@crema/utility/IntlMessages';
+import React from 'react';
+import CancelButton from '../../../@softbd/elements/button/CancelButton/CancelButton';
+import EditButton from '../../../@softbd/elements/button/EditButton/EditButton';
+import {Grid} from '@mui/material';
+import DetailsInputView from '../../../@softbd/elements/display/DetailsInputView/DetailsInputView';
+import DecoratedRowStatus from '../../../@softbd/elements/display/DecoratedRowStatus/DecoratedRowStatus';
+
+interface IContactInfoDetails {
+  contactInfoId: number;
+  onClose: () => void;
+  openEditModal: (id: number) => void;
+}
+
+const IndustryAssociationContactInfoDetailsPopup = ({
+  contactInfoId,
+  openEditModal,
+  ...props
+}: IContactInfoDetails) => {
+  const {data: contactInfo, isLoading} = useFetchContactInfo(contactInfoId);
+
+  const {messages} = useIntl();
+
+  return (
+    <CustomDetailsViewMuiModal
+      open={true}
+      {...props}
+      title={
+        <>
+          <BusinessCenter />
+          <IntlMessages id='common.contact_office' />
+        </>
+      }
+      actions={
+        <>
+          <CancelButton onClick={props.onClose} isLoading={isLoading} />
+          <EditButton onClick={() => openEditModal(contactInfoId)} />
+        </>
+      }>
+      <Grid container spacing={5}>
+        <Grid item xs={6}>
+          <DetailsInputView
+            label={messages['common.title']}
+            value={contactInfo?.title}
+            isLoading={isLoading}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <DetailsInputView
+            label={messages['common.title_en']}
+            value={contactInfo?.title_en}
+            isLoading={isLoading}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <DetailsInputView
+            label={messages['common.email']}
+            value={contactInfo?.email}
+            isLoading={isLoading}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <DetailsInputView
+            label={messages['common.phone_number']}
+            value={contactInfo?.phone}
+            isLoading={isLoading}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <DetailsInputView
+            label={messages['common.mobile']}
+            value={contactInfo?.mobile}
+            isLoading={isLoading}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <DetailsInputView
+            label={messages['common.status']}
+            value={<DecoratedRowStatus rowStatus={contactInfo?.row_status} />}
+            isLoading={isLoading}
+          />
+        </Grid>
+      </Grid>
+    </CustomDetailsViewMuiModal>
+  );
+};
+
+export default IndustryAssociationContactInfoDetailsPopup;
--- services/IndustryAssociationManagement/hooks.tsx
+++ services/IndustryAssociationManagement/hooks.tsx
@@ -1,6 +1,17 @@
 import {useAxiosSWR} from '../../@softbd/hooks/useAxiosSWR';
-import {API_INDUSTRY_ASSOCIATIONS} from '../../@softbd/common/apiRoutes';
+import {
+  API_INDUSTRY_ASSOCIATION_CONTACT_INFO,
+  API_INDUSTRY_ASSOCIATIONS,
+} from '../../@softbd/common/apiRoutes';
 
 export function useFetchIndustryAssociations(params: any) {
   return useAxiosSWR([API_INDUSTRY_ASSOCIATIONS, params]);
 }
+
+export const useFetchContactInfo = (contactInfoId: number | null) => {
+  return useAxiosSWR(
+    contactInfoId
+      ? API_INDUSTRY_ASSOCIATION_CONTACT_INFO + '/' + contactInfoId
+      : null,
+  );
+};
