1885f001231dfcc5dd594f2a2b4b44d73b94c898
AfrarJahin <afrarjahin@gmail.com>
2022-02-03 15:01:30 +0600

    feat: button enable disable functionality in hrdemand manage pop up

--- services/instituteManagement/HrDemandService.ts
+++ services/instituteManagement/HrDemandService.ts
@@ -1,5 +1,8 @@
 import {apiPut} from '../../@softbd/common/api';
-import {API_HUMAN_RESOURCE_DEMAND_APPROVED_BY_INSTITUTE} from '../../@softbd/common/apiRoutes';
+import {
+  API_HUMAN_RESOURCE_DEMAND_APPROVED_BY_INSTITUTE,
+  API_HUMAN_RESOURCE_DEMAND_REJECTED_BY_INSTITUTE,
+} from '../../@softbd/common/apiRoutes';
 import {catchBlockHandler} from '../../@softbd/utilities/helpers';
 
 export const updateHrDemand = async (hrDemandId: number | null, data: any) => {
@@ -7,6 +10,16 @@ export const updateHrDemand = async (hrDemandId: number | null, data: any) => {
     let response: any = await apiPut(
       API_HUMAN_RESOURCE_DEMAND_APPROVED_BY_INSTITUTE + '/' + hrDemandId,
       data,
+    );
+    return response.data;
+  } catch (error) {
+    catchBlockHandler(error);
+  }
+};
+export const rejectHrdemand = async (hrDemandId: number | null) => {
+  try {
+    let response: any = await apiPut(
+      API_HUMAN_RESOURCE_DEMAND_REJECTED_BY_INSTITUTE + '/' + hrDemandId,
     );
     return response.data;
   } catch (error) {
