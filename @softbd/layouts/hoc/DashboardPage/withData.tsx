54670efbbe2c4fe6173dbbd1ed6ab9d6cf35955c
Milton Deb <miltondeb.cse.bgc@gmail.com>
2022-02-08 11:42:27 +0600

    landing page search implement

--- @softbd/layouts/hoc/DashboardPage/withData.tsx
+++ @softbd/layouts/hoc/DashboardPage/withData.tsx
@@ -3,26 +3,27 @@ import {useSelector} from 'react-redux';
 import Loader from '../../../../@crema/core/Loader';
 import {AppState} from '../../../../redux/store';
 import {getSSOLoginUrl} from '../../../common/SSOConfig';
-import {youthDomain} from "../../../common/constants";
+import {youthDomain} from '../../../common/constants';
+import {LINK_FRONTEND_YOUTH_FEED} from '../../../common/appLinks';
 
 const withData = (ComposedComponent: any) => (props: any) => {
-    const {user, loading} = useSelector<AppState, AppState['auth']>(
-        ({auth}) => auth,
-    );
-    // const {asPath} = useRouter();
-    // const queryParams = asPath.split('?')[1];
-    useEffect(() => {
-        if (!user && !loading) {
-            // Router.push('/signin' + (queryParams ? '?' + queryParams : ''));
-            window.location.href = getSSOLoginUrl();
-        }
-        if (user && user.isYouthUser) {
-            window.location.href = youthDomain();
-        }
-    }, [user, loading]);
-    if (!user || loading) return <Loader/>;
+  const {user, loading} = useSelector<AppState, AppState['auth']>(
+    ({auth}) => auth,
+  );
+  // const {asPath} = useRouter();
+  // const queryParams = asPath.split('?')[1];
+  useEffect(() => {
+    if (!user && !loading) {
+      // Router.push('/signin' + (queryParams ? '?' + queryParams : ''));
+      window.location.href = getSSOLoginUrl();
+    }
+    if (user && user.isYouthUser) {
+      window.location.href = youthDomain() + LINK_FRONTEND_YOUTH_FEED;
+    }
+  }, [user, loading]);
+  if (!user || loading) return <Loader />;
 
-    return <ComposedComponent {...props} />;
+  return <ComposedComponent {...props} />;
 };
 
 export default withData;
