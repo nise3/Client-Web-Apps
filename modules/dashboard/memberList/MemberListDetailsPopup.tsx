f9e325c741ab442a4ebeba9cb4113ae610cb06b5
shakib77 <shakibshaon77@gmail.com>
2022-02-10 16:59:40 +0600

    fix: added member details page missing field

--- modules/dashboard/memberList/MemberListDetailsPopup.tsx
+++ modules/dashboard/memberList/MemberListDetailsPopup.tsx
@@ -9,6 +9,7 @@ import IconList from '../../../@softbd/icons/IconList';
 import DoneIcon from '@mui/icons-material/Done';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {useFetchOrganization} from '../../../services/organaizationManagement/hooks';
+import ImageView from '../../../@softbd/elements/display/ImageView/ImageView';
 
 type Props = {
   itemId: number;
@@ -58,11 +59,34 @@ const MemberListDetailsPopup = ({itemId, ...props}: Props) => {
               isLoading={isLoading}
             />
           </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.title_en']}
+              value={itemData?.title_en}
+              isLoading={isLoading}
+            />
+          </Grid>
 
           <Grid item xs={12} md={6}>
             <DetailsInputView
               label={messages['menu.organization_type']}
               value={itemData?.organization_type_title}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.industry_association_sub_trade']}
+              value={(itemData?.sub_trades || [])
+                .map((subTrade: any) => subTrade?.title)
+                .join(', ')}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.memberId']}
+              value={itemData?.id}
               isLoading={isLoading}
             />
           </Grid>
@@ -82,6 +106,97 @@ const MemberListDetailsPopup = ({itemId, ...props}: Props) => {
           </Grid>
           <Grid item xs={12} md={6}>
             <DetailsInputView
+              label={messages['common.fax_no']}
+              value={itemData?.fax_no}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.name_of_the_office_head']}
+              value={itemData?.name_of_the_office_head}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.name_of_the_office_head_en']}
+              value={itemData?.name_of_the_office_head_en}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.name_of_the_office_head_designation']}
+              value={itemData?.name_of_the_office_head_designation}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.name_of_the_office_head_designation_en']}
+              value={itemData?.name_of_the_office_head_designation_en}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.contact_person_name']}
+              value={itemData?.contact_person_name}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.contact_person_name_en']}
+              value={itemData?.contact_person_name_en}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.contact_person_mobile']}
+              value={itemData?.contact_person_mobile}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.contact_person_email']}
+              value={itemData?.contact_person_email}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.contact_person_designation']}
+              value={itemData?.contact_person_designation}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.contact_person_designation_en']}
+              value={itemData?.contact_person_designation_en}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.description']}
+              value={itemData?.description}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.description_en']}
+              value={itemData?.description_en}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
               label={messages['divisions.label']}
               value={itemData?.loc_division_title}
               isLoading={isLoading}
@@ -96,8 +211,50 @@ const MemberListDetailsPopup = ({itemId, ...props}: Props) => {
           </Grid>
           <Grid item xs={12} md={6}>
             <DetailsInputView
+              label={messages['upazilas.label']}
+              value={itemData?.loc_upazila_title}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
               label={messages['common.address']}
               value={itemData?.address}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.address_en']}
+              value={itemData?.address_en}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.location_latitude']}
+              value={itemData?.location_latitude}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.location_longitude']}
+              value={itemData?.location_longitude}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DetailsInputView
+              label={messages['common.google_map_src']}
+              value={itemData?.google_map_src}
+              isLoading={isLoading}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <ImageView
+              label={messages['common.logo']}
+              imageUrl={itemData?.logo}
               isLoading={isLoading}
             />
           </Grid>
