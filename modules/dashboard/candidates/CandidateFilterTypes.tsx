a943530d1e3257ff51bbba6a8125e0639fdef25b
Milton Deb <miltondeb.cse.bgc@gmail.com>
2022-02-14 13:38:28 +0600

    job interview recruitment step

---
+++ modules/dashboard/candidates/CandidateFilterTypes.tsx
@@ -0,0 +1,13 @@
+export enum CandidateFilterTypes {
+  ALL = 'all',
+  NOT_VIEWED = 'not_viewed',
+  VIEWED = 'viewed',
+  REJECTED = 'rejected',
+  QUALIFIED = 'qualified',
+  SHORTLISTED = 'shortlisted',
+  SCHEDULED = 'scheduled',
+  INTERVIEWED = 'interviewed',
+  HIRE_SELECTED = 'hire_selected',
+  HIRE_INVITED = 'hire_invited',
+  HIRED = 'hired',
+}
