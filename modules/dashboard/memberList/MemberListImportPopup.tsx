32453eea16a46566b99e68973947f94d25a3575a
ehsan munna <grmunnabd@gmail.com>
2022-02-14 12:47:24 +0600

    initial modal added

---
+++ modules/dashboard/memberList/MemberListImportPopup.tsx
@@ -0,0 +1,85 @@
+import {Grid, Typography} from '@mui/material';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import React, {FC} from 'react';
+import HookFormMuiModal from '../../../@softbd/modals/HookFormMuiModal/HookFormMuiModal';
+import CancelButton from '../../../@softbd/elements/button/CancelButton/CancelButton';
+import SubmitButton from '../../../@softbd/elements/button/SubmitButton/SubmitButton';
+import {useIntl} from 'react-intl';
+import IntlMessages from '../../../@crema/utility/IntlMessages';
+import FileUploadComponent from '../../filepond/FileUploadComponent';
+import {isBreakPointUp} from '../../../@crema/utility/Utils';
+
+interface MemberImportPopupProps {
+  onClose: () => void;
+  userData: any;
+  refreshDataTable: () => void;
+}
+
+
+const MemberImportPopup: FC<MemberImportPopupProps> = ({
+  refreshDataTable,
+  ...props
+}) => {
+  // const authUser = useAuthUser();
+  const {messages} = useIntl();
+  const {
+    // control,
+    register,
+    // reset,
+    // getValues,
+    handleSubmit,
+    // setError,
+    setValue,
+    formState: {errors, isSubmitting},
+  } = useForm<any>();
+  const onSubmit: SubmitHandler<any> = async (data: any) => {
+    console.log('submit data: ', data);
+  };
+
+  return (
+    <HookFormMuiModal
+      open={true}
+      {...props}
+      title={
+        <>
+          <IntlMessages
+            id='common.edit'
+            values={{
+              subject: <IntlMessages id='common.profile' />,
+            }}
+          />
+        </>
+      }
+      maxWidth={isBreakPointUp('xl') ? 'lg' : 'md'}
+      handleSubmit={handleSubmit(onSubmit)}
+      actions={
+        <>
+          <CancelButton onClick={props.onClose} isLoading={false} />
+          <SubmitButton isSubmitting={isSubmitting} isLoading={false} />
+        </>
+      }>
+      <Grid container spacing={3} sx={{overflow: 'hidden'}}>
+        <Grid item xs={12}>
+          <Typography variant={'h6'}>
+            {messages['association.association_information']}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <FileUploadComponent
+            id='logo'
+            // defaultFileUrl={userData?.logo}
+            errorInstance={errors}
+            setValue={setValue}
+            register={register}
+            label={messages['common.image_path']}
+            required={false}
+          />
+        </Grid>
+
+
+      </Grid>
+    </HookFormMuiModal>
+  );
+};
+export default MemberImportPopup;
