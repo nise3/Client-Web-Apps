4a0255f014e237e1403a22f5a48756ad35634e8e
AfrarJahin <afrarjahin@gmail.com>
2022-02-15 15:11:03 +0600

    fix: design issue on memberlist import file

--- modules/dashboard/memberList/MemberListImportPopup.tsx
+++ modules/dashboard/memberList/MemberListImportPopup.tsx
@@ -1,4 +1,4 @@
-import {Grid, Input, Link, Typography} from '@mui/material';
+import {Grid, Link} from '@mui/material';
 import {SubmitHandler, useForm} from 'react-hook-form';
 import React, {FC} from 'react';
 import HookFormMuiModal from '../../../@softbd/modals/HookFormMuiModal/HookFormMuiModal';
@@ -8,30 +8,37 @@ import IntlMessages from '../../../@crema/utility/IntlMessages';
 import {isBreakPointUp} from '../../../@crema/utility/Utils';
 import CommonButton from '../../../@softbd/elements/button/CommonButton/CommonButton';
 import {createExcelImport} from '../../../services/IndustryManagement/FileExportImportService';
-
+import CustomTextInput from '../../../@softbd/elements/input/CustomTextInput/CustomTextInput';
+import {processServerSideErrors} from '../../../@softbd/utilities/validationErrorHandler';
+import useNotiStack from '../../../@softbd/hooks/useNotifyStack';
+import {useIntl} from 'react-intl';
+import DownloadIcon from '@mui/icons-material/Download';
 interface MemberImportPopupProps {
   onClose: () => void;
   userData: any;
   refreshDataTable: () => void;
 }
 
-
 const MemberImportPopup: FC<MemberImportPopupProps> = ({
   refreshDataTable,
   ...props
 }) => {
+  const {messages} = useIntl();
+  const {errorStack} = useNotiStack();
   const {
+    register,
     handleSubmit,
+    // errors,
+    setError,
     formState: {isSubmitting},
   } = useForm<any>();
   const onSubmit: SubmitHandler<any> = async (data: any) => {
     try {
-
-        await createExcelImport(data);
-        props.onClose();
-        refreshDataTable();
+      await createExcelImport(data.file[0]);
+      props.onClose();
+      refreshDataTable();
     } catch (error: any) {
-      // processServerSideErrors({error, setError});
+      processServerSideErrors({error, setError, errorStack});
     }
   };
 
@@ -41,6 +48,7 @@ const MemberImportPopup: FC<MemberImportPopupProps> = ({
       {...props}
       title={
         <>
+          <DownloadIcon />
           <IntlMessages
             id='common.add_new'
             values={{
@@ -59,32 +67,32 @@ const MemberImportPopup: FC<MemberImportPopupProps> = ({
       }>
       <Grid container spacing={3} sx={{overflow: 'hidden'}}>
         <Grid item xs={6}>
-          <Typography variant={'h6'}>
-            Downlod sample excel file
-          </Typography>
-          <Link href="../../../public/template/organization-list.xlsx" download>
+          <Link href='../../../public/template/organization-list.xlsx' download>
             <CommonButton
               key={1}
               onClick={() => console.log('download file')}
-              btnText={"Download"}
+              btnText={messages['common.download_excel_file'] as string}
+              variant={'outlined'}
+              color={'primary'}
             />
           </Link>
-
         </Grid>
 
         <Grid item xs={6}>
-          <Typography variant={'h6'}>
-            Upload excel file
-          </Typography>
-          <label htmlFor="contained-button-file">
+          <CustomTextInput
+            id='file'
+            name='file'
+            label={messages['common.file_upload']}
+            register={register}
+            type={'file'}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          {/* <label htmlFor="contained-button-file">
             <Input id={'fileinput'} name={'file'} type="file" />
-            {/*<Button variant="contained" component="span">*/}
-            {/*  Upload*/}
-            {/*</Button>*/}
-          </label>
+          </label> */}
         </Grid>
-
-
       </Grid>
     </HookFormMuiModal>
   );
