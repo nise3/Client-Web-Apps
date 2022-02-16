9f475bc2356b977bce52a96e50cbe0c55316112a
shakib77 <shakibshaon77@gmail.com>
2022-02-16 17:18:10 +0600

    feat: working on interview process schedule

--- modules/dashboard/candidates/InterviewManagementPage.tsx
+++ modules/dashboard/candidates/InterviewManagementPage.tsx
@@ -7,6 +7,11 @@ import {useFetchIndustryAssociationRecruitmentStepCandidateList} from '../../../
 import CandidateComponent from './CandidateComponent';
 import NoDataFoundComponent from '../../youth/common/NoDataFoundComponent';
 import ContentWithImageSkeleton from '../../youth/profile/component/ContentWithImageSkeleton';
+import {useIntl} from 'react-intl';
+import ScheduleCreateComponentPopup from './ScheduleCreateComponent';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import ScheduleListComponentPopup from './ScheduleListComponent';
 
 const StyledBox = styled(Box)(({theme}) => ({
   '& .CandidateComponent-root:not(:first-of-type)': {
@@ -19,10 +24,25 @@ interface InterviewManagementPageProps {
 }
 
 const InterviewManagementPage = ({jobId}: InterviewManagementPageProps) => {
+  const {messages} = useIntl();
+
   const [currentStep, setCurrentStep] = useState<any>(null);
   const [nextStep, setNextStep] = useState<any>(null);
   const [candidatesFilter, setCandidatesFilter] = useState<any>(null);
   const [reloadStepData, setReloadStepData] = useState<any>(false);
+
+  const [openCreateSchedulePopup, setOpenCreateSchedulePopup] =
+    useState<boolean>(false);
+  const [openScheduleListPopup, setOpenScheduleListPopup] =
+    useState<boolean>(false);
+
+  const closeCreateScheduleAddEditPopup = () => {
+    setOpenCreateSchedulePopup(false);
+  };
+
+  const closeScheduleListPopup = () => {
+    setOpenScheduleListPopup(false);
+  };
 
   const {
     data: candidateList,
@@ -63,9 +83,41 @@ const InterviewManagementPage = ({jobId}: InterviewManagementPageProps) => {
         </Box>
       ) : (
         <React.Fragment>
-          <H4 sx={{fontWeight: 'bold', marginBottom: '20px'}}>
-            {(candidateList || []).length} candidates{' '}
-          </H4>
+          <Grid container>
+            <Grid item xs={6}>
+              <H4 sx={{fontWeight: 'bold', marginBottom: '20px'}}>
+                {(candidateList || []).length}{' '}
+                {messages['common.candidates'] as string}{' '}
+              </H4>
+            </Grid>
+            <Grid item xs={3}>
+              <Button
+                variant='contained'
+                onClick={() => setOpenCreateSchedulePopup(true)}>
+                Create Schedule
+              </Button>
+              {openCreateSchedulePopup && (
+                <ScheduleCreateComponentPopup
+                  jobId={jobId}
+                  onClose={closeCreateScheduleAddEditPopup}
+                />
+              )}
+            </Grid>
+            <Grid item xs={3}>
+              <Button
+                variant='contained'
+                onClick={() => setOpenScheduleListPopup(true)}>
+                Schedule List
+              </Button>
+              {openScheduleListPopup && (
+                <ScheduleListComponentPopup
+                  onClose={closeScheduleListPopup}
+                  jobId={jobId}
+                  currentStep={currentStep}
+                />
+              )}
+            </Grid>
+          </Grid>
 
           {(candidateList || []).map((candidate: any) => (
             <CandidateComponent
---
+++ modules/dashboard/candidates/ScheduleCreateComponent.tsx
@@ -0,0 +1,189 @@
+import IntlMessages from '../../../@crema/utility/IntlMessages';
+import {isBreakPointUp} from '../../../@crema/utility/Utils';
+import CancelButton from '../../../@softbd/elements/button/CancelButton/CancelButton';
+import SubmitButton from '../../../@softbd/elements/button/SubmitButton/SubmitButton';
+import {Grid} from '@mui/material';
+import FormRadioButtons from '../../../@softbd/elements/input/CustomRadioButtonGroup/FormRadioButtons';
+import CustomTextInput from '../../../@softbd/elements/input/CustomTextInput/CustomTextInput';
+import HookFormMuiModal from '../../../@softbd/modals/HookFormMuiModal/HookFormMuiModal';
+import React, {useMemo} from 'react';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import {useIntl} from 'react-intl';
+import {yupResolver} from '@hookform/resolvers/yup';
+import yup from '../../../@softbd/libs/yup';
+import CustomDateTimeField from '../../../@softbd/elements/input/CustomDateTimeField';
+import CustomTimePicker from '../../../@softbd/elements/input/TimePicker';
+import {
+  createCandidateStepSchedule,
+  updateCandidateStepSchedule,
+} from '../../../services/IndustryManagement/IndustryAssociationService';
+import useNotiStack from '../../../@softbd/hooks/useNotifyStack';
+import useSuccessMessage from '../../../@softbd/hooks/useSuccessMessage';
+import {processServerSideErrors} from '../../../@softbd/utilities/validationErrorHandler';
+
+interface IScheduleCreateComponentPopupProps {
+  onClose: () => void;
+  scheduleId?: number | null;
+  jobId: string;
+  schedule?: any;
+}
+
+const ScheduleCreateComponentPopup = ({
+  scheduleId,
+  jobId,
+  schedule,
+  ...props
+}: IScheduleCreateComponentPopupProps) => {
+  const {messages} = useIntl();
+
+  const {errorStack} = useNotiStack();
+  const {createSuccessMessage, updateSuccessMessage} = useSuccessMessage();
+
+  const isEdit = scheduleId != null;
+
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      venue: yup
+        .string()
+        .required()
+        .label(messages['common.venue'] as string),
+    });
+  }, [messages]);
+
+  const {
+    control,
+    register,
+    setError,
+    // reset,
+    handleSubmit,
+    formState: {errors, isSubmitting},
+  } = useForm<any>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const notifyApplicant = useMemo(
+    () => [
+      {
+        key: '1',
+        label: messages['common.yes'],
+      },
+      {
+        key: '2',
+        label: messages['common.no'],
+      },
+    ],
+    [messages],
+  );
+
+  const onSubmit: SubmitHandler<any> = async (formData: any) => {
+    // formData.start = formData.start_date;
+    // if (formData.start_time) {
+    //   formData.start_time = hasSecond(formData.start_time)
+    //     ? formData.start_time
+    //     : `${formData.start_time}:00`;
+    // }
+
+    try {
+      formData.job_id = jobId;
+
+      if (scheduleId) {
+        await updateCandidateStepSchedule(scheduleId, formData);
+        updateSuccessMessage('common.interview_schedule');
+        // mutateSteps();
+      } else {
+        await createCandidateStepSchedule(formData);
+        createSuccessMessage('common.interview_schedule');
+      }
+      props.onClose();
+      // mutateSteps();
+    } catch (error: any) {
+      processServerSideErrors({error, setError, validationSchema, errorStack});
+    }
+  };
+
+  return (
+    <HookFormMuiModal
+      open={true}
+      {...props}
+      title={
+        <>
+          {isEdit ? (
+            <IntlMessages
+              id='common.edit'
+              values={{
+                subject: <IntlMessages id='common.interview_schedule' />,
+              }}
+            />
+          ) : (
+            <IntlMessages
+              id='common.add_new'
+              values={{
+                subject: <IntlMessages id='common.interview_schedule' />,
+              }}
+            />
+          )}
+        </>
+      }
+      maxWidth={isBreakPointUp('xl') ? 'lg' : 'md'}
+      handleSubmit={handleSubmit(onSubmit)}
+      actions={
+        <>
+          <CancelButton onClick={props.onClose} isLoading={false} />
+          <SubmitButton isSubmitting={isSubmitting} isLoading={false} />
+        </>
+      }>
+      <Grid container spacing={5}>
+        <Grid item xs={6}>
+          <CustomDateTimeField
+            required
+            id='date'
+            label={messages['common.date']}
+            register={register}
+            errorInstance={errors}
+            isLoading={false}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <CustomTimePicker
+            id='time'
+            label={messages['common.time']}
+            register={register}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <CustomTextInput
+            required
+            id='maximum_number_of_applicants'
+            label={messages['common.maximum_number_of_applicants']}
+            register={register}
+            errorInstance={errors}
+            isLoading={false}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <CustomTextInput
+            required
+            id='interview_address'
+            label={messages['common.venue']}
+            register={register}
+            errorInstance={errors}
+            isLoading={false}
+            placeholder='Type a venue address'
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormRadioButtons
+            id='interview_invite_type'
+            required
+            label={'common.invite_type'}
+            radios={notifyApplicant}
+            control={control}
+            isLoading={false}
+          />
+        </Grid>
+      </Grid>
+    </HookFormMuiModal>
+  );
+};
+
+export default ScheduleCreateComponentPopup;
