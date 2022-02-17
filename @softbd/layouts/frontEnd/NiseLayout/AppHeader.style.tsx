a4f2ba6f77b369375b54a7116312539a017cf03d
Milton Deb <miltondeb.cse.bgc@gmail.com>
2022-02-17 12:32:56 +0600

    design issue fix

--- @softbd/layouts/frontEnd/NiseLayout/AppHeader.style.tsx
+++ @softbd/layouts/frontEnd/NiseLayout/AppHeader.style.tsx
@@ -80,7 +80,7 @@ export const StyledAppBar = styled(AppBar)(({theme}) => ({
   },
 
   [`& .${classes.menuItem}`]: {
-    fontSize: '1.15rem',
+    fontSize: '1.13rem',
     padding: '20px 10px',
     color: '#746F6F',
     '&>a': {
