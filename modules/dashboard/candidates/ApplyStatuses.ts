ec86c152910d769178c4edd8791a915dfb899300
shakib77 <shakibshaon77@gmail.com>
2022-02-24 20:12:33 +0600

    Interview schedule create, assign, unassign and interview process done

--- modules/dashboard/candidates/ApplyStatuses.ts
+++ modules/dashboard/candidates/ApplyStatuses.ts
@@ -2,7 +2,7 @@ export enum ApplyStatuses {
   APPLIED = 1,
   REJECTED = 2,
   SHORTLISTED = 3,
-  Interview_scheduled = 4,
+  INTERVIEW_SCHEDULED = 4,
   INTERVIEW_INVITED = 5,
   INTERVIEWED = 6,
   HIRING_LISTED = 7,
