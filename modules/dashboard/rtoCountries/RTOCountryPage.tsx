08e4464d536d185ca0af40b122e299ec37ee9d89
Asir053 <asirbillah@gmail.com>
2022-02-28 13:43:24 +0600

    retained previous values for multiselect checkbox for countries

--- modules/dashboard/rtoCountries/RTOCountryPage.tsx
+++ modules/dashboard/rtoCountries/RTOCountryPage.tsx
@@ -2,51 +2,32 @@ import React, { useCallback, useMemo, useState } from "react";
 import PageBlock from "../../../@softbd/utilities/PageBlock";
 import AddButton from "../../../@softbd/elements/button/AddButton/AddButton";
 import { useIntl } from "react-intl";
-/*import ReadButton from '../../../@softbd/elements/button/ReadButton/ReadButton';
-import EditButton from '../../../@softbd/elements/button/EditButton/EditButton';
-import DatatableButtonGroup from '../../../@softbd/elements/button/DatatableButtonGroup/DatatableButtonGroup';*/
 import useReactTableFetchData from "../../../@softbd/hooks/useReactTableFetchData";
 import { API_RTO_COUNTRIES } from "../../../@softbd/common/apiRoutes";
 import ReactTable from "../../../@softbd/table/Table/ReactTable";
 import RTOCountryAddEditPopup from "./RTOCountryAddEditPopup";
-import RTOCountryDetailsPopup from "./RTOCountryDetailsPopup";
+
 
 import IntlMessages from "../../../@crema/utility/IntlMessages";
-//import useNotiStack from '../../../@softbd/hooks/useNotifyStack';
-//import {isResponseSuccess} from '../../../@softbd/utilities/helpers';
 import IconCountry from "../../../@softbd/icons/IconCountry";
 import LocaleLanguage from "../../../@softbd/utilities/LocaleLanguage";
 
 const RTOCountryPage = () => {
   const {messages, locale} = useIntl();
-  //const {successStack} = useNotiStack();
 
-  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
   const [isOpenAddEditModal, setIsOpenAddEditModal] = useState(false);
-  const [isOpenDetailsModal, setIsOpenDetailsModal] = useState(false);
+
   const [isToggleTable, setIsToggleTable] = useState<boolean>(false);
 
   const closeAddEditModal = useCallback(() => {
     setIsOpenAddEditModal(false);
-    setSelectedItemId(null);
+
   }, []);
 
   const openAddEditModal = useCallback((itemId: number | null = null) => {
-    setIsOpenDetailsModal(false);
+
     setIsOpenAddEditModal(true);
-    setSelectedItemId(itemId);
-  }, []);
 
-  /*const openDetailsModal = useCallback(
-    (itemId: number) => {
-      setIsOpenDetailsModal(true);
-      setSelectedItemId(itemId);
-    },
-    [selectedItemId],
-  );*/
-
-  const closeDetailsModal = useCallback(() => {
-    setIsOpenDetailsModal(false);
   }, []);
 
 
@@ -91,7 +72,7 @@ const RTOCountryPage = () => {
       <PageBlock
         title={
           <>
-            <IconCountry /> <IntlMessages id='country.label' />
+            <IconCountry /> <IntlMessages id='common.rto-countries' />
           </>
         }
         extra={[
@@ -103,7 +84,7 @@ const RTOCountryPage = () => {
               <IntlMessages
                 id={'common.add_new'}
                 values={{
-                  subject: messages['rto-rtoCountries.label'],
+                  subject: messages['rto-country.label'],
                 }}
               />
             }
@@ -126,14 +107,6 @@ const RTOCountryPage = () => {
           />
         )}
 
-        {isOpenDetailsModal && selectedItemId && (
-          <RTOCountryDetailsPopup
-            key={1}
-            itemId={selectedItemId}
-            onClose={closeDetailsModal}
-            openEditModal={openAddEditModal}
-          />
-        )}
       </PageBlock>
     </>
   );
