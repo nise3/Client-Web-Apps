edc37d7413d2cd1b5bcd2e18860141f42fc53859
Animesh Karmakar <animesh.pust@gmail.com>
2022-02-28 12:36:35 +0600

    feat: total employee style changed.

--- services/IndustryManagement/NascibMemberRegistrationService.ts
+++ services/IndustryManagement/NascibMemberRegistrationService.ts
@@ -1,11 +1,11 @@
 import {apiPost} from '../../@softbd/common/api';
 import {catchBlockHandler} from '../../@softbd/utilities/helpers';
-import {API_NASCIB_MEMBER_PUBLIC_REGISTRATION} from '../../@softbd/common/apiRoutes';
+import {API__PUBLIC_NASCIB_MEMBER_REGISTRATION} from '../../@softbd/common/apiRoutes';
 
 export const registerNASCIBMember = async (data: any) => {
   try {
     let response: any = await apiPost(
-      API_NASCIB_MEMBER_PUBLIC_REGISTRATION,
+      API__PUBLIC_NASCIB_MEMBER_REGISTRATION,
       data,
     );
     return response.data;
