a81f2b644f4f8b8cd2feaa61c6d02dda7cfaa7d4
Animesh Karmakar <animesh.pust@gmail.com>
2022-03-06 18:18:31 +0600

    fix: design of nascib registration form. v.1

--- modules/industry/enrollment/index.tsx
+++ modules/industry/enrollment/index.tsx
@@ -908,7 +908,7 @@ const NASCIBMemberRegistrationForm: FC<NASCIBMemberRegistrationFormProps> = ({
                     </p>
                   </StyledHeader>
                   <Grid item xs={12}>
-                    <Grid container spacing={4} p={2}>
+                    <Grid container spacing={2} p={2}>
                       <Grid item xs={6}>
                         <CustomTextInput
                           required
