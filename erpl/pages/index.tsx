f25fd2df9feebdb41376eeb26c8b516606c05ac7
ehsan munna <grmunnabd@gmail.com>
2022-03-09 12:33:35 +0600

    coverage and showintype added

--- erpl/pages/index.tsx
+++ erpl/pages/index.tsx
@@ -2,8 +2,8 @@ import ErplDefaultFrontPage from '../../@softbd/layouts/hoc/ErplDefaultFrontPage
 import React from 'react';
 import asyncComponent from '../../@crema/utility/asyncComponent';
 
-const Industry = asyncComponent(() => import('../../modules/industry'));
+const Erpl = asyncComponent(() => import('../../modules/erpl'));
 
 export default ErplDefaultFrontPage(() => {
-  return <Industry />;
+  return <Erpl />;
 });
