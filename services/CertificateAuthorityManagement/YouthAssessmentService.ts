e3d005a93cab9e9668d9a14d27b7e483cbc547fb
AfrarJahin <afrarjahin@gmail.com>
2022-03-13 18:10:27 +0600

    feat: youth assessment route changed

--- services/CertificateAuthorityManagement/YouthAssessmentService.ts
+++ services/CertificateAuthorityManagement/YouthAssessmentService.ts
@@ -1,25 +1,30 @@
-import { IYouthAssessment } from "../../shared/Interface/common.interface";
-import { apiPost } from "../../@softbd/common/api";
-import { API_PUBLIC_YOUTH_ASSESSMENTS, API_YOUTH_ASSESSMENT } from "../../@softbd/common/apiRoutes";
-import { catchBlockHandler } from "../../@softbd/utilities/helpers";
-import { IAssessmentBatchAssign } from "../../shared/Interface/assessmentManagement.interface";
+import {IYouthAssessment} from '../../shared/Interface/common.interface';
+import {apiPost} from '../../@softbd/common/api';
+import {
+  API_RPL_APPLICATION,
+  API_YOUTH_ASSESSMENT,
+} from '../../@softbd/common/apiRoutes';
+import {catchBlockHandler} from '../../@softbd/utilities/helpers';
+import {IAssessmentBatchAssign} from '../../shared/Interface/assessmentManagement.interface';
 
 export const createYouthAssessment = async (data: IYouthAssessment) => {
   try {
-    let response: any = await apiPost(API_PUBLIC_YOUTH_ASSESSMENTS, data);
+    let response: any = await apiPost(API_YOUTH_ASSESSMENT, data);
     return response.data;
   } catch (error) {
     catchBlockHandler(error);
   }
 };
 
-
 export const assessmentAssignBatch = async (
   data: IAssessmentBatchAssign,
   youth_assessment_id: number | null,
 ) => {
   try {
-    let response: any = await apiPost(API_YOUTH_ASSESSMENT+ '/' +youth_assessment_id + '/assign-to-batch', data);
+    let response: any = await apiPost(
+      API_RPL_APPLICATION + '/' + youth_assessment_id + '/assign-to-batch',
+      data,
+    );
     return response.data;
   } catch (error) {
     catchBlockHandler(error);
