03f38b602a8def6b08c051ec1be962be1408e1da
Milton Deb <miltondeb.cse.bgc@gmail.com>
2022-03-14 11:07:59 +0600

    build error fix and package json update

--- @softbd/layouts/frontEnd/ErplDefaultLayout/Footer.tsx
+++ @softbd/layouts/frontEnd/ErplDefaultLayout/Footer.tsx
@@ -2,21 +2,7 @@ import React from 'react';
 import {styled} from '@mui/material/styles';
 import {Box, Container, Grid, Typography} from '@mui/material';
 import {useIntl} from 'react-intl';
-import {
-  LINK_FRONTEND_INDUSTRY_CONTACT,
-  LINK_FRONTEND_INDUSTRY_FAQS,
-  LINK_FRONTEND_JOBS,
-  LINK_INDUSTRY_FRONTEND_STATIC_CONTENT,
-} from '../../../common/appLinks';
-import {
-  CONTENT_ID_ABOUT_US,
-  CONTENT_ID_GUIDELINES,
-  CONTENT_ID_PRIVACY_POLICY,
-  CONTENT_ID_TERMS_AND_CONDITIONS,
-} from '../../../utilities/StaticContentConfigs';
 import GoToTop from '../../../../modules/goToTop';
-import {Link} from '../../../elements/common';
-import {ArrowForwardIos} from '@mui/icons-material';
 
 const PREFIX = 'Footer';
 
@@ -65,94 +51,6 @@ const Footer = () => {
           container
           spacing={10}
           sx={{display: 'flex', justifyContent: 'space-around'}}>
-          {/* <Grid item xs={6} md={4}>
-            <Typography variant='subtitle2' gutterBottom={true}>
-              <Box component={'span'} fontWeight='fontWeightBold'>
-                {messages['footer.important_links']}
-              </Box>
-            </Typography>
-            <Box display='flex' mt={2} justifyContent='space-between'>
-              <Box>
-                <Link
-                  href={
-                    LINK_INDUSTRY_FRONTEND_STATIC_CONTENT + CONTENT_ID_ABOUT_US
-                  }
-                  className={classes.bullet}>
-                  <ArrowForwardIos
-                    sx={{fontSize: '0.625rem', marginRight: '2px'}}
-                    className={classes.primary}
-                  />{' '}
-                  {messages['footer.about_us']}
-                </Link>
-                <Link
-                  href={
-                    LINK_INDUSTRY_FRONTEND_STATIC_CONTENT +
-                    CONTENT_ID_TERMS_AND_CONDITIONS
-                  }
-                  className={classes.bullet}>
-                  <ArrowForwardIos
-                    sx={{fontSize: '0.625rem', marginRight: '2px'}}
-                    className={classes.primary}
-                  />{' '}
-                  {messages['footer.terms_and_condition']}
-                </Link>
-                <Link
-                  href={
-                    LINK_INDUSTRY_FRONTEND_STATIC_CONTENT +
-                    CONTENT_ID_PRIVACY_POLICY
-                  }
-                  className={classes.bullet}>
-                  <ArrowForwardIos
-                    sx={{fontSize: '0.625rem', marginRight: '2px'}}
-                    className={classes.primary}
-                  />{' '}
-                  {messages['footer.privacy_policy']}
-                </Link>
-                <Link
-                  href={LINK_FRONTEND_INDUSTRY_CONTACT}
-                  className={classes.bullet}>
-                  <ArrowForwardIos
-                    sx={{fontSize: '0.625rem', marginRight: '2px'}}
-                    className={classes.primary}
-                  />{' '}
-                  {messages['footer.contact']}
-                </Link>
-              </Box>
-              <Box>
-                <Link
-                  target={'_blank'}
-                  href={LINK_FRONTEND_JOBS}
-                  className={classes.bullet}>
-                  <ArrowForwardIos
-                    sx={{fontSize: '0.625rem', marginRight: '2px'}}
-                    className={classes.primary}
-                  />{' '}
-                  {messages['job_post.label']}
-                </Link>
-                <Link
-                  href={LINK_FRONTEND_INDUSTRY_FAQS}
-                  className={classes.bullet}>
-                  <ArrowForwardIos
-                    sx={{fontSize: '0.625rem', marginRight: '2px'}}
-                    className={classes.primary}
-                  />{' '}
-                  {messages['footer.faq']}
-                </Link>
-                <Link
-                  href={
-                    LINK_INDUSTRY_FRONTEND_STATIC_CONTENT +
-                    CONTENT_ID_GUIDELINES
-                  }
-                  className={classes.bullet}>
-                  <ArrowForwardIos
-                    sx={{fontSize: '0.625rem', marginRight: '2px'}}
-                    className={classes.primary}
-                  />{' '}
-                  {messages['footer.guideline']}
-                </Link>
-              </Box>
-            </Box>
-          </Grid> */}
           <Grid item md={12}>
             <Grid container columnSpacing={12}>
               <Grid item md={8}>
@@ -166,7 +64,7 @@ const Footer = () => {
                   href='https://a2i.gov.bd/'
                   rel='noopener noreferrer'>
                   <Box component={'span'}>
-                  <img
+                    <img
                       src={'/tecnical.png'}
                       alt='crema-logo'
                       className={classes.softbdImage}
