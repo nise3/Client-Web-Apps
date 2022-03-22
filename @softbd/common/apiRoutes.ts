5f7ee4ca2d0e5579e957581ae4ec78172bce55f7
AfrarJahin <afrarjahin@gmail.com>
2022-03-22 18:28:43 +0600

    feat: combined progress report getlist api integrated

--- @softbd/common/apiRoutes.ts
+++ @softbd/common/apiRoutes.ts
@@ -167,6 +167,11 @@ export const API_RTO_BATCH = INSTITUTE_SERVICE_PATH + '/rto-batches';
 
 /**             eRPL End         */
 
+/**training center report start*/
+export const API_TRAINING_CENTERS_REPORTING_COMBINED_PROGRESS =
+  INSTITUTE_SERVICE_PATH + '/training-centers/reporting/combined-progress';
+/**training center report end*/
+
 /** All institute service privates routes section end */
 
 /** All institute service public routes section start */
