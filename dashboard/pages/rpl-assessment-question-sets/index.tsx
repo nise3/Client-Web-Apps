b3d79bdf0defd11c4bf85f055b4f3106f236972b
Milton Deb <miltondeb.cse.bgc@gmail.com>
2022-03-29 17:49:02 +0600

    rpl route change and file name change

---
+++ dashboard/pages/rpl-assessment-question-sets/index.tsx
@@ -0,0 +1,21 @@
+import asyncComponent from '../../../@crema/utility/asyncComponent';
+import PageMeta from '../../../@crema/core/PageMeta';
+import DashboardPage from '../../../@softbd/layouts/hoc/DashboardPage';
+import React from 'react';
+import {useIntl} from 'react-intl';
+
+const RPLAssessmentQuestionSetPage = asyncComponent(
+  () =>
+    import(
+      '../../../modules/dashboard/rplAssessmentQuestionSets/RPLAssessmentQuestionSetPage'
+    ),
+);
+export default DashboardPage(() => {
+  const {messages} = useIntl();
+  return (
+    <>
+      <PageMeta title={messages['subject.label']} />
+      <RPLAssessmentQuestionSetPage />
+    </>
+  );
+});
