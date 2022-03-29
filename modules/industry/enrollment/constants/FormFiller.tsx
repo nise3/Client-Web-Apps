0de904e4b372fa6f19d0ce5d19a13ab7c3f0d74d
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2022-03-29 10:13:15 +0600

    SMEF registration form api integration and check

--- modules/industry/enrollment/constants/FormFiller.tsx
+++ modules/industry/enrollment/constants/FormFiller.tsx
@@ -2,6 +2,7 @@ enum FormFiller {
   SELF = '1',
   UDC_ENTREPRENEUR = '2',
   CHAMBER_ASSOCIATION = '3',
+  SME_CLUSTER = '4',
 }
 
 export default FormFiller;
--- modules/industry/memberRegistration/index.tsx
+++ modules/industry/memberRegistration/index.tsx
@@ -1,6 +1,7 @@
 import {useMemo} from 'react';
 import NASCIBMemberRegistrationForm from '../enrollment';
 import MemberRegistration from './MemberRegistration';
+import SMEFMemberRegistrationForm from '../enrollment/SMEFMemberRegistrationForm';
 
 const RegistrationForm = () => {
   const NASCIBDomains = useMemo(() => {
@@ -11,11 +12,27 @@ const RegistrationForm = () => {
     ];
   }, []);
 
-  return window && NASCIBDomains.includes(String(window.location.origin)) ? (
-    <NASCIBMemberRegistrationForm />
-  ) : (
-    <MemberRegistration />
-  );
+  const SMEFDomains = useMemo(() => {
+    return [
+      'https://smef.nise.gov.bd',
+      'http://smef.nise.asm',
+      'https://smef-dev.nise3.xyz',
+    ];
+  }, []);
+
+  if (window && NASCIBDomains.includes(String(window.location.origin))) {
+    return <NASCIBMemberRegistrationForm />;
+  } else if (window && SMEFDomains.includes(String(window.location.origin))) {
+    return <SMEFMemberRegistrationForm />;
+  } else {
+    return <MemberRegistration />;
+  }
+
+  /*  return window && NASCIBDomains.includes(String(window.location.origin)) ? (
+      <NASCIBMemberRegistrationForm />
+    ) : (
+      <MemberRegistration />
+    );*/
 };
 
 export default RegistrationForm;
