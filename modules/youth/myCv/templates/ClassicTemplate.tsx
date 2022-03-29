c958dd7ed109cc96e00aa00a5539deee32ab77f8
ehsan munna <grmunnabd@gmail.com>
2022-03-29 12:40:49 +0600

    code cleanup unused code removed

--- modules/youth/myCv/templates/ClassicTemplate.tsx
+++ modules/youth/myCv/templates/ClassicTemplate.tsx
@@ -23,10 +23,6 @@ interface ClassicTemplateProps {
 }
 
 const ClassicTemplate: FC<ClassicTemplateProps> = ({userData}) => {
-  // const LanguageProficiencyType: any = {
-  //   '1': 'Easily',
-  //   '2': 'Not Easily',
-  // };
 
   const setPhoto = (data: any) => {
     var elem = document.getElementById('photo') as Element;
@@ -52,6 +48,7 @@ const ClassicTemplate: FC<ClassicTemplateProps> = ({userData}) => {
     return {
       dom: {
         elem: languageHead,
+        // @ts-ignore
         position: languageHead?.getBBox()
       },
       textBox: tspan.getBBox(),
@@ -90,28 +87,7 @@ const ClassicTemplate: FC<ClassicTemplateProps> = ({userData}) => {
     return addresstxt;
   };
 
-  /*const educationText = (education: any, locale: string) => {
-    return (
-      (education?.institute_name
-        ? `${messages['common.institute_name']}: ${education[getProps('institute_name', locale)]}, `
-        : ' ') +
-      (education?.duration
-        ? `${messages['education.duration']}: ${parseFloat(education?.duration)} yrs,`
-        : ' ') +
-      (education?.result?.code === 'GRADE'
-        ? education?.cgpa
-          ? `${messages['education.cgpa']}: ` + parseFloat(education?.cgpa) + ' ( out of ' + parseInt(education?.cgpa_scale) + ' ), '
-          : ' '
-        : `${messages['education.result']}: ` + education?.result?.title) +
-      (education?.year_of_passing
-        ? `${messages['education.passing_year']}: ` + parseInt(education?.year_of_passing) + ', '
-        : ' ')
-    );
-  };*/
-
   const renderSVG = (language: string, node?: any) => {
-    // console.log(`renderSVG: ${language}`)
-    // if (node == undefined) {
     let exNode = document.getElementById('svg-div');
     if (exNode) {
       node.removeChild(exNode);
@@ -122,7 +98,6 @@ const ClassicTemplate: FC<ClassicTemplateProps> = ({userData}) => {
     div.setAttribute('id', 'svg-div');
     div.innerHTML = pageSVG;
 
-    // console.log('PAGE >> ', pageSVG);
     node.appendChild(div);
     const svgNode = div.children[0];
     const rects = svgNode.querySelectorAll('g[id]>text');
@@ -145,7 +120,6 @@ const ClassicTemplate: FC<ClassicTemplateProps> = ({userData}) => {
 
     let cvHeader = setHeaderLanguage('cv-header', 'personal_info.curriculum_vitae');
     let {dom: { position }} = cvHeader;
-    // console.log('{position} ', position);
     d3.select('#cv-header')
       .append('path')
       .attr('fill', '#231f20')
@@ -155,118 +129,12 @@ const ClassicTemplate: FC<ClassicTemplateProps> = ({userData}) => {
     setAreaText(svgNode, 'email', userData?.email, 'lt');
 
     setAreaText(svgNode, 'address', addressText(userData, language));
-
-    // setHeaderLanguage('objective-headling', "common.objective");
-    // setAreaText(svgNode, 'objective', userData[getProps('bio', language)]);
-    // setHeaderLanguage('jobexperiance-headling', "common.job_experience");
-    // const experiance = setAreaText(
-    //   svgNode,
-    //   'experience',
-    //   userData?.youth_job_experiences.map((experience: any) => {
-    //     return (
-    //       (experience?.company_name
-    //         ? `${messages['common.company_name_bn']}: ` + experience[getProps('company_name', language)] + ', '
-    //         : ' ') +
-    //       (experience?.position
-    //         ? `${messages['common.position']}: ` + experience[getProps('position', language)] + ', '
-    //         : ' ') +
-    //       (experience?.start_date
-    //         ? 'Start Date: ' +
-    //         new Date(experience?.start_date).toLocaleDateString('en-US') +
-    //         ', '
-    //         : ' ') +
-    //       (experience?.is_currently_working === 1
-    //         ? 'Currently working here' + ', '
-    //         : 'End Date: ' +
-    //         new Date(experience?.end_date).toLocaleDateString('en-US'))
-    //     );
-    //   }),
-    // );
-    // setHeaderAndLinePosition(experiance, 'education-headling', 'education-headling-line', 'education');
-    // setHeaderLanguage('education-headling', "common.educations");
-    // const education = setAreaText(
-    //   svgNode,
-    //   'education',
-    //   userData?.youth_educations.map((education: any) => {
-    //     return educationText(education, language);
-    //   }),
-    //   'lt',
-    // );
-    // setHeaderAndLinePosition(education, 'computerskill-headling', 'computerskill-headling-line', 'computerskill')
-    // setHeaderLanguage('computerskill-headling', "common.skills");
-    // const skill = setAreaText(
-    //   svgNode,
-    //   'computerskill',
-    //   userData?.skills.map((skill: any, index: number) => {
-    //     return skill?.title ? skill[getProps('title', language)] as any + ' ' : ' ';
-    //   }),
-    // );
-    // let languageReact = setHeaderAndLinePosition(skill, 'language-headling', 'language-headling-line', 'language');
-    // setHeaderLanguage('language-headling', "language.proficiency");
-    // setAreaText(
-    //   svgNode,
-    //   'language',
-    //   userData?.youth_languages_proficiencies.map((language: any) => {
-    //     return (
-    //       (language?.language_title
-    //         ? `${messages['common.language']}: ` + language?.language_title + ', '
-    //         : ' ') +
-    //       (language?.reading_proficiency_level
-    //         ? `${messages['language.read']}: ` +
-    //         LanguageProficiencyType[language?.reading_proficiency_level] +
-    //         ', '
-    //         : ' ') +
-    //       (language?.speaking_proficiency_level
-    //         ? `${messages['language.speak']}: ` +
-    //         LanguageProficiencySpeakingType[
-    //         language?.speaking_proficiency_level
-    //         ] +
-    //         ', '
-    //         : ' ')
-    //     );
-    //   }),
-    // );
-    // //@ts-ignore
-    // const langulageRect = languageReact.rectCord?.children[0].getBBox();
-    // const languageLastBoxBottomY = langulageRect.y + langulageRect.height;
-    // const bottomPadding = 20;
-
-    // // update svg if less then last cord
-    // let svg = document.getElementById('svg') as Element;
-    // svg.setAttribute('viewBox', `0 0 595.276 ${languageLastBoxBottomY + bottomPadding}`);
-
-    // let cvBody = document.getElementById('classic-data') as HTMLElement;
     getStructureData(userData, messages, language);
   };
 
-  /*const LanguageProficiencySpeakingType: any = {
-    '1': 'Fluently',
-    '2': 'Not Fluently',
-  };*/
-  // const chk = useIntl();
   const {messages, locale} = useIntl();
-  // const [currentLang, setCurrentLang] = useState("");
-  // let svgNodeRef: HTMLElement;
-  // console.log(messages, locale)
-  // const theCB = useCallback((node) => {
   const theCB = useCallback(
     (node: any) => {
-      // console.log('inside CB: ', locale);
-
-      // const language = locale;
-      // setCurrentLang(language);
-      // console.log('locale: ', locale);
-      // console.log('currentLang: ', currentLang);
-      // console.log('is not same lang: ', locale !== currentLang);
-      // if (node && locale !== currentLang) {
-      //   svgNodeRef = node;
-      // } else {
-      //   node = svgNodeRef;
-      //   let exNode = document.getElementById("svg-div");
-      //   if(exNode)
-      //     node.removeChild(exNode);
-      // }
-      // console.log('node.children.length: ', node.children.length)
       if (!node || node.children.length > 0) return;
       renderSVG(locale, node);
     },
@@ -274,14 +142,8 @@ const ClassicTemplate: FC<ClassicTemplateProps> = ({userData}) => {
   );
 
   useEffect(() => {
-    // console.log('1. inside effect ', locale);
-    // setLanguage(locale);
-    // console.log('2. inside effect ', language);
-    // console.log('svgNodeRef before assign ', svgNodeRef);
-    // theCB(svgNodeRef);
     let exNode = document.getElementById('svgBox');
     renderSVG(locale, exNode);
-    // console.log('asd;flasjf', exNode)
   }, [locale]);
 
   return (
