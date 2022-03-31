e674039011a09640f97d67ab8737319337e6d8c2
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2022-03-31 17:30:55 +0600

    AvatarImageView component is created

---
+++ @softbd/elements/display/ImageView/AvatarImageView.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {FILE_SERVER_FILE_VIEW_ENDPOINT} from '../../../common/apiRoutes';
+import {Avatar} from '@mui/material';
+
+interface AvatarImageViewProps {
+  src: string;
+  sx?: any;
+  alt?: string;
+  className?: string;
+  variant?: any;
+  title?: string;
+}
+
+const AvatarImageView = ({
+  src,
+  sx,
+  alt,
+  className,
+  variant,
+  title,
+}: AvatarImageViewProps) => {
+  const absoluteSrc = FILE_SERVER_FILE_VIEW_ENDPOINT + src || '';
+
+  return (
+    <Avatar
+      className={className}
+      src={absoluteSrc}
+      variant={variant}
+      alt={alt ? alt : 'Image'}
+      sx={sx}
+      title={title}
+    />
+  );
+};
+
+export default AvatarImageView;
