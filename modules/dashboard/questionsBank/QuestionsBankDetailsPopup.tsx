5daf7fecec2a78e20744fe67c2d1261bc09748a0
shakib77 <shakibshaon77@gmail.com>
2022-04-03 13:15:13 +0600

    feat: exam question Bank crud is completed

--- modules/dashboard/questionsBank/QuestionsBankDetailsPopup.tsx
+++ modules/dashboard/questionsBank/QuestionsBankDetailsPopup.tsx
@@ -9,6 +9,7 @@ import EditButton from '../../../@softbd/elements/button/EditButton/EditButton';
 import {Grid} from '@mui/material';
 import DetailsInputView from '../../../@softbd/elements/display/DetailsInputView/DetailsInputView';
 import React from 'react';
+import {QuestionType} from './QuestionBanksEnums';
 
 interface IProps {
   itemId: number;
@@ -52,6 +53,28 @@ const QuestionsBankDetailsPopup = ({
             <Grid container spacing={5}>
               <Grid item xs={12}>
                 <DetailsInputView
+                  label={messages['subject.label']}
+                  value={itemData?.exam_subject_title}
+                  isLoading={isLoading}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container spacing={5}>
+              <Grid item xs={12}>
+                <DetailsInputView
+                  label={messages['question.type']}
+                  value={itemData?.question_type}
+                  isLoading={isLoading}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container spacing={5}>
+              <Grid item xs={12}>
+                <DetailsInputView
                   label={messages['common.title']}
                   value={itemData?.title}
                   isLoading={isLoading}
@@ -70,136 +93,113 @@ const QuestionsBankDetailsPopup = ({
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['subject.label']}
-                  value={itemData?.subject_title}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['question.type']}
-                  value={itemData?.type}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
 
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option1']}
-                  value={itemData?.option_1}
-                  isLoading={isLoading}
-                />
+          {QuestionType.MCQ == itemData?.question_type && (
+            <>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option1']}
+                      value={itemData?.option_1}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option1_en']}
+                      value={itemData?.option_1_en}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option2']}
+                      value={itemData?.option_2}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option2_en']}
+                      value={itemData?.option_2_en}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option3']}
+                      value={itemData?.option_3}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option3_en']}
+                      value={itemData?.option_3_en}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option4']}
+                      value={itemData?.option_4}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12}>
+                    <DetailsInputView
+                      label={messages['option.option4_en']}
+                      value={itemData?.option_4_en}
+                      isLoading={isLoading}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </>
+          )}
+          {(QuestionType.MCQ == itemData.question_type ||
+            QuestionType.YES_NO == itemData.question_type) && (
+            <Grid item xs={6}>
+              <Grid container spacing={5}>
+                <Grid item xs={12}>
+                  <DetailsInputView
+                    label={messages['question.answer']}
+                    value={itemData?.answer}
+                    isLoading={isLoading}
+                  />
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option1_en']}
-                  value={itemData?.option_1_en}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option2']}
-                  value={itemData?.option_2}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option2_en']}
-                  value={itemData?.option_2_en}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option3']}
-                  value={itemData?.option_3}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option3_en']}
-                  value={itemData?.option_3_en}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option4']}
-                  value={itemData?.option_4}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['option.option4_en']}
-                  value={itemData?.option_4_en}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <DetailsInputView
-                  label={messages['question.answer']}
-                  value={itemData?.answer}
-                  isLoading={isLoading}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
+          )}
         </Grid>
       </CustomDetailsViewMuiModal>
     </>
