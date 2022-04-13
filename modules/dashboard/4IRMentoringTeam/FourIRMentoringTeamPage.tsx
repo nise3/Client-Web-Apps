db5f699f3213651cf91d0f5811393586df346763
Sourav <golamkibriashourav@gmail.com>
2022-04-13 14:36:47 +0600

    Mentoring team ui

---
+++ modules/dashboard/4IRMentoringTeam/FourIRMentoringTeamPage.tsx
@@ -0,0 +1,165 @@
+import React, {useCallback, useMemo, useState} from 'react';
+import PageBlock from '../../../@softbd/utilities/PageBlock';
+import AddButton from '../../../@softbd/elements/button/AddButton/AddButton';
+import {useIntl} from 'react-intl';
+import ReadButton from '../../../@softbd/elements/button/ReadButton/ReadButton';
+import EditButton from '../../../@softbd/elements/button/EditButton/EditButton';
+import DeleteButton from '../../../@softbd/elements/button/DeleteButton/DeleteButton';
+import DatatableButtonGroup from '../../../@softbd/elements/button/DatatableButtonGroup/DatatableButtonGroup';
+import useReactTableFetchData from '../../../@softbd/hooks/useReactTableFetchData';
+import ReactTable from '../../../@softbd/table/Table/ReactTable';
+import {getCalculatedSerialNo} from '../../../@softbd/utilities/helpers';
+import FourIRMentoringTeamAddEditPopup from './FourIRMentoringTeamAddEditPopup';
+import FourIRMentoringTeamDetailsPopup from './FourIRMentoringTeamDetailsPopup';
+
+import IntlMessages from '../../../@crema/utility/IntlMessages';
+import useNotiStack from '../../../@softbd/hooks/useNotifyStack';
+
+import IconBranch from '../../../@softbd/icons/IconBranch';
+
+const FourIRImplemntingTeamPage = () => {
+  const {messages, locale} = useIntl();
+  const {successStack} = useNotiStack();
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+  const [isOpenAddEditModal, setIsOpenAddEditModal] = useState(false);
+  const [isOpenDetailsModal, setIsOpenDetailsModal] = useState(false);
+  const [isToggleTable, setIsToggleTable] = useState<boolean>(false);
+  const closeAddEditModal = useCallback(() => {
+    setIsOpenAddEditModal(false);
+    setSelectedItemId(null);
+  }, []);
+
+  const openAddEditModal = useCallback((itemId: number | null = null) => {
+    setIsOpenDetailsModal(false);
+    setIsOpenAddEditModal(true);
+    setSelectedItemId(itemId);
+  }, []);
+
+  const openDetailsModal = useCallback(
+    (itemId: number) => {
+      setIsOpenDetailsModal(true);
+      setSelectedItemId(itemId);
+    },
+    [selectedItemId],
+  );
+
+  const closeDetailsModal = useCallback(() => {
+    setIsOpenDetailsModal(false);
+  }, []);
+
+  const refreshDataTable = useCallback(() => {}, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: '#',
+        disableFilters: true,
+        disableSortBy: true,
+        Cell: (props: any) => {
+          return getCalculatedSerialNo(
+            props.row.index,
+            props.currentPageIndex,
+            props.currentPageSize,
+          );
+        },
+      },
+
+      {
+        Header: messages['common.name'],
+      },
+      {
+        Header: messages['common.email'],
+      },
+      {
+        Header: messages['common.phone_number'],
+      },
+      {
+        Header: messages['role.label'],
+        isVisible: false,
+      },
+      {
+        Header: messages['common.designation'],
+        isVisible: false,
+      },
+
+      {
+        Header: messages['common.actions'],
+        Cell: (props: any) => {
+          //   let data = props.row.original;
+          return (
+            <DatatableButtonGroup>
+              <ReadButton onClick={() => {}} />
+              <EditButton onClick={() => {}} />
+              <DeleteButton
+                deleteAction={() => {}}
+                deleteTitle={messages['common.delete_confirm'] as string}
+              />
+            </DatatableButtonGroup>
+          );
+        },
+        sortable: false,
+      },
+    ],
+    [messages, locale],
+  );
+
+  const {onFetchData, data, loading, pageCount, totalCount} =
+    useReactTableFetchData({
+      urlPath: './4ir_mentoring_team',
+    });
+
+  return (
+    <>
+      <PageBlock
+        title={
+          <>
+            <IconBranch /> <IntlMessages id='4ir.mentoring_team' />
+          </>
+        }
+        extra={[
+          <AddButton
+            key={1}
+            onClick={() => openAddEditModal(null)}
+            isLoading={false}
+            tooltip={
+              <IntlMessages
+                id={'common.add_new'}
+                values={{
+                  subject: messages['4ir.mentoring_team'],
+                }}
+              />
+            }
+          />,
+        ]}>
+        <ReactTable
+          columns={columns}
+          data={data}
+          fetchData={onFetchData}
+          loading={loading}
+          pageCount={pageCount}
+          totalCount={totalCount}
+          toggleResetTable={isToggleTable}
+        />
+        {isOpenAddEditModal && (
+          <FourIRMentoringTeamAddEditPopup
+            key={1}
+            onClose={closeAddEditModal}
+            itemId={selectedItemId}
+            refreshDataTable={refreshDataTable}
+          />
+        )}
+
+        {isOpenDetailsModal && selectedItemId && (
+          <FourIRMentoringTeamDetailsPopup
+            key={1}
+            itemId={selectedItemId}
+            onClose={closeDetailsModal}
+            openEditModal={openAddEditModal}
+          />
+        )}
+      </PageBlock>
+    </>
+  );
+};
+
+export default FourIRImplemntingTeamPage;
