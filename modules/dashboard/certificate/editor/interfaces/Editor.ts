7a525a1523a206a8c557e693e39deddf06e2adae
Kalyan Halder <kalyan@softbdltd.com>
2022-04-26 04:45:27 +0000

    Revert "Merge branch 'feat/certificate_editor_intial-Sourav' into 'develop'"
    This reverts merge request !1004

--- modules/dashboard/certificate/editor/interfaces/Editor.ts
+++
@@ -1,9 +0,0 @@
-export enum EditorPanel {
-  Settings = "settings-panel",
-  Text = "text-panel",
-  Elements = "elements-panel",
-  Image = "image-panel",
-  ImageProperties = "image-properties-panel",
-  TextProperties = "text-properties-panel",
-  RectangleProperties = "rectangle-properties-panel",
-}
--- modules/dashboard/certificate/editor/utils/uuid.ts
+++
@@ -1,7 +0,0 @@
-import { v4 } from "uuid";
-
-export function uuid(namespace?: string) {
-  const id = v4();
-
-  return namespace ? `${namespace}:${id}` : id;
-}
