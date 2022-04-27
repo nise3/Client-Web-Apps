051fa8e0868fb9acf781c5832fe68853ad5212e2
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2022-04-27 09:21:55 +0600

    working on stepper

---
+++ modules/dashboard/4IRGuideline/4IRGuidelineAddEditPopup.tsx
@@ -0,0 +1,234 @@
+import {useEffect} from 'react';
+import yup from '../../../@softbd/libs/yup';
+import {Grid} from '@mui/material';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import React, {FC, useMemo} from 'react';
+import HookFormMuiModal from '../../../@softbd/modals/HookFormMuiModal/HookFormMuiModal';
+import CustomTextInput from '../../../@softbd/elements/input/CustomTextInput/CustomTextInput';
+import SubmitButton from '../../../@softbd/elements/button/SubmitButton/SubmitButton';
+import {useIntl} from 'react-intl';
+
+import IntlMessages from '../../../@crema/utility/IntlMessages';
+import CancelButton from '../../../@softbd/elements/button/CancelButton/CancelButton';
+import IconBranch from '../../../@softbd/icons/IconBranch';
+import {isBreakPointUp} from '../../../@crema/utility/Utils';
+import CustomDateTimeField from '../../../@softbd/elements/input/CustomDateTimeField';
+import FileUploadComponent from '../../filepond/FileUploadComponent';
+import {
+  createTNAReport,
+  updateTNAReport,
+} from '../../../services/4IRManagement/TNAReportServices';
+import {processServerSideErrors} from '../../../@softbd/utilities/validationErrorHandler';
+import useSuccessMessage from '../../../@softbd/hooks/useSuccessMessage';
+import useNotiStack from '../../../@softbd/hooks/useNotifyStack';
+import {useFetchTNAReport} from '../../../services/instituteManagement/hooks';
+
+interface FourIRGuideLineAddEditPopupProps {
+  itemId: number | null;
+  fourIRProjectId: number;
+  onClose: () => void;
+  refreshDataTable: () => void;
+}
+
+const initialValues = {
+  workshop_name: '',
+  skill_required: '',
+  start_date: '',
+  end_date: '',
+  venue: '',
+  file_path: '',
+};
+
+const FourIRGuideLineAddEditPopup: FC<FourIRGuideLineAddEditPopupProps> = ({
+  itemId,
+  fourIRProjectId,
+  refreshDataTable,
+
+  ...props
+}) => {
+  const {messages} = useIntl();
+  const {errorStack} = useNotiStack();
+  const isEdit = itemId != null;
+
+  const {createSuccessMessage, updateSuccessMessage} = useSuccessMessage();
+
+  const {
+    data: itemData,
+    isLoading,
+    mutate: mutateTNAReport,
+  } = useFetchTNAReport(itemId);
+
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      workshop_name: yup
+        .string()
+        .title()
+        .required()
+        .label(messages['common.workshop_name'] as string),
+      skill_required: yup
+        .string()
+        .title()
+        .required()
+        .label(messages['common.required_skill'] as string),
+      start_date: yup
+        .string()
+        .title()
+        .required()
+        .matches(/(19|20)\d\d-[01]\d-[0123]\d/)
+        .label(messages['common.start_date'] as string),
+      end_date: yup
+        .string()
+        .title()
+        .required()
+        .matches(/(19|20)\d\d-[01]\d-[0123]\d/)
+        .label(messages['common.end_date'] as string),
+      venue: yup.string().label(messages['common.venue'] as string),
+      file_path: yup
+        .string()
+        .required()
+        .label(messages['common.file_path'] as string),
+    });
+  }, [messages]);
+
+  const {
+    //    control,
+    register,
+    reset,
+    setError,
+    setValue,
+    handleSubmit,
+    formState: {errors, isSubmitting},
+  } = useForm<any>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  useEffect(() => {
+    if (itemData) {
+      reset({
+        workshop_name: itemData.workshop_name,
+        skill_required: itemData.skill_required,
+        start_date: itemData.start_date,
+        end_date: itemData.end_date,
+        venue: itemData.venue,
+        file_path: itemData.file_path,
+      });
+    } else reset(initialValues);
+  }, [itemData]);
+
+  const onSubmit: SubmitHandler<any> = async (data: any) => {
+    try {
+      let payload = {
+        four_ir_project_id: fourIRProjectId,
+        ...data,
+      };
+
+      if (itemId !== null) {
+        await updateTNAReport(payload, itemId);
+        updateSuccessMessage('4ir.TNA_report');
+        mutateTNAReport();
+      } else {
+        await createTNAReport(payload);
+        createSuccessMessage('4ir.TNA_report');
+      }
+      props.onClose();
+      refreshDataTable();
+    } catch (error: any) {
+      processServerSideErrors({error, setError, validationSchema, errorStack});
+    }
+  };
+
+  return (
+    <HookFormMuiModal
+      open={true}
+      {...props}
+      title={
+        <>
+          <IconBranch />
+          {isEdit ? (
+            <IntlMessages
+              id='common.edit'
+              values={{subject: <IntlMessages id='4ir.TNA_report' />}}
+            />
+          ) : (
+            <IntlMessages
+              id='common.add_new'
+              values={{
+                subject: <IntlMessages id='4ir.TNA_report' />,
+              }}
+            />
+          )}
+        </>
+      }
+      maxWidth={isBreakPointUp('xl') ? 'lg' : 'md'}
+      handleSubmit={handleSubmit(onSubmit)}
+      actions={
+        <>
+          <CancelButton onClick={props.onClose} isLoading={isLoading} />
+          <SubmitButton isSubmitting={isSubmitting} isLoading={isLoading} />
+        </>
+      }>
+      <Grid container spacing={5}>
+        <Grid item xs={12} md={6}>
+          <CustomTextInput
+            required
+            id='workshop_name'
+            label={messages['common.workshop_name']}
+            register={register}
+            errorInstance={errors}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <CustomTextInput
+            id='skill_required'
+            label={messages['common.required_skill']}
+            register={register}
+            errorInstance={errors}
+            isLoading={false}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <CustomDateTimeField
+            id='start_date'
+            label={messages['common.start_date']}
+            register={register}
+            errorInstance={errors}
+            isLoading={false}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <CustomDateTimeField
+            id='end_date'
+            label={messages['common.end_date']}
+            register={register}
+            errorInstance={errors}
+            isLoading={false}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} alignSelf='center'>
+          <CustomTextInput
+            required
+            id='venue'
+            label={messages['common.venue']}
+            register={register}
+            errorInstance={errors}
+            isLoading={false}
+            rows={3}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FileUploadComponent
+            id='file_path'
+            errorInstance={errors}
+            setValue={setValue}
+            register={register}
+            sizeLimitText={'3MB'}
+            label={messages['common.project_upload']}
+            required={false}
+          />
+        </Grid>
+      </Grid>
+    </HookFormMuiModal>
+  );
+};
+export default FourIRGuideLineAddEditPopup;
---
+++ modules/dashboard/4IRGuideline/4IRGuidelinePage.tsx
@@ -0,0 +1,203 @@
+import React, {useCallback, useMemo, useState} from 'react';
+import PageBlock from '../../../@softbd/utilities/PageBlock';
+import AddButton from '../../../@softbd/elements/button/AddButton/AddButton';
+import {useIntl} from 'react-intl';
+import ReadButton from '../../../@softbd/elements/button/ReadButton/ReadButton';
+import EditButton from '../../../@softbd/elements/button/EditButton/EditButton';
+import DeleteButton from '../../../@softbd/elements/button/DeleteButton/DeleteButton';
+import DatatableButtonGroup from '../../../@softbd/elements/button/DatatableButtonGroup/DatatableButtonGroup';
+import useReactTableFetchData from '../../../@softbd/hooks/useReactTableFetchData';
+import ReactTable from '../../../@softbd/table/Table/ReactTable';
+import {
+  getCalculatedSerialNo,
+  isResponseSuccess,
+} from '../../../@softbd/utilities/helpers';
+import FourIRTNAReportAddEditPopup from './FourIRTNAReportAddEditPopup';
+import FourIRTNAReportDetailsPopup from './FourIRTNAReportDetailsPopup';
+import {API_4IR_TNA_REPORT} from '../../../@softbd/common/apiRoutes';
+import IntlMessages from '../../../@crema/utility/IntlMessages';
+import useNotiStack from '../../../@softbd/hooks/useNotifyStack';
+
+import IconBranch from '../../../@softbd/icons/IconBranch';
+import {deleteTNAReport} from '../../../services/4IRManagement/TNAReportServices';
+
+interface IFourIRImplemntingTeamPage {
+  fourIRProjectId: number;
+}
+
+const FourGuidelinePage = ({
+  fourIRProjectId = 9,
+}: IFourIRImplemntingTeamPage) => {
+  const {messages, locale} = useIntl();
+  const {successStack} = useNotiStack();
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+  const [isOpenAddEditModal, setIsOpenAddEditModal] = useState(false);
+  const [isOpenDetailsModal, setIsOpenDetailsModal] = useState(false);
+  const [isToggleTable, setIsToggleTable] = useState<boolean>(false);
+  const closeAddEditModal = useCallback(() => {
+    setIsOpenAddEditModal(false);
+    setSelectedItemId(null);
+  }, []);
+
+  const openAddEditModal = useCallback((itemId: number | null = null) => {
+    setIsOpenDetailsModal(false);
+    setIsOpenAddEditModal(true);
+    setSelectedItemId(itemId);
+  }, []);
+
+  const openDetailsModal = useCallback(
+    (itemId: number) => {
+      setIsOpenDetailsModal(true);
+      setSelectedItemId(itemId);
+    },
+    [selectedItemId],
+  );
+
+  const closeDetailsModal = useCallback(() => {
+    setIsOpenDetailsModal(false);
+  }, []);
+
+  const refreshDataTable = useCallback(() => {
+    setIsToggleTable((prev) => !prev);
+  }, []);
+
+  const deleteTNAReportItem = async (itemId: number) => {
+    let response = await deleteTNAReport(itemId);
+
+    if (isResponseSuccess(response)) {
+      successStack(
+        <IntlMessages
+          id='common.subject_deleted_successfully'
+          values={{subject: <IntlMessages id='4ir.label' />}}
+        />,
+      );
+
+      refreshDataTable();
+    }
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: '#',
+        disableFilters: true,
+        disableSortBy: true,
+        Cell: (props: any) => {
+          return getCalculatedSerialNo(
+            props.row.index,
+            props.currentPageIndex,
+            props.currentPageSize,
+          );
+        },
+      },
+
+      {
+        Header: messages['common.workshop_name'],
+        accessor: 'workshop_name',
+      },
+      {
+        Header: messages['common.required_skill'],
+        accessor: 'skill_required',
+      },
+      {
+        Header: messages['common.start_date'],
+        accessor: 'start_date',
+      },
+      {
+        Header: messages['common.end_date'],
+        accessor: 'end_date',
+        isVisible: false,
+      },
+      {
+        Header: messages['common.venue'],
+        accessor: 'venue',
+        isVisible: false,
+      },
+
+      {
+        Header: messages['common.actions'],
+        Cell: (props: any) => {
+          let data = props.row.original;
+
+          return (
+            <DatatableButtonGroup>
+              <ReadButton onClick={() => openDetailsModal(data.id)} />
+              <EditButton onClick={() => openAddEditModal(data.id)} />
+              <DeleteButton
+                deleteAction={() => deleteTNAReportItem(data.id)}
+                deleteTitle={messages['common.delete_confirm'] as string}
+              />
+            </DatatableButtonGroup>
+          );
+        },
+        sortable: false,
+      },
+    ],
+    [messages, locale],
+  );
+
+  const {onFetchData, data, loading, pageCount, totalCount} =
+    useReactTableFetchData({
+      urlPath: API_4IR_TNA_REPORT,
+      paramsValueModifier: (params: any) => {
+        params['four_ir_project_id'] = fourIRProjectId;
+        return params;
+      },
+    });
+
+  return (
+    <>
+      <PageBlock
+        title={
+          <>
+            <IconBranch /> <IntlMessages id='4ir.TNA_report' />
+          </>
+        }
+        extra={[
+          <AddButton
+            key={1}
+            onClick={() => openAddEditModal(null)}
+            isLoading={false}
+            tooltip={
+              <IntlMessages
+                id={'common.add_new'}
+                values={{
+                  subject: messages['4ir.TNA_report'],
+                }}
+              />
+            }
+          />,
+        ]}>
+        <ReactTable
+          columns={columns}
+          data={data}
+          fetchData={onFetchData}
+          loading={loading}
+          pageCount={pageCount}
+          totalCount={totalCount}
+          toggleResetTable={isToggleTable}
+        />
+        {isOpenAddEditModal && (
+          <FourIRTNAReportAddEditPopup
+            key={1}
+            onClose={closeAddEditModal}
+            itemId={selectedItemId}
+            fourIRProjectId={fourIRProjectId}
+            refreshDataTable={refreshDataTable}
+          />
+        )}
+
+        {isOpenDetailsModal && selectedItemId && (
+          <FourIRTNAReportDetailsPopup
+            key={1}
+            itemId={selectedItemId}
+            onClose={closeDetailsModal}
+            openEditModal={openAddEditModal}
+          />
+        )}
+      </PageBlock>
+    </>
+  );
+};
+
+export default FourGuidelinePage;
