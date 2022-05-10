14fdfa8c2d529a74ab5c10cff3a572c06de113e7
Milton Deb <miltondeb.cse.bgc@gmail.com>
2022-05-10 18:37:29 +0600

    cdap login commented

--- @softbd/elements/button/GotoSignInOrUpMenu/GotoSignInOrUpMenu.tsx
+++ @softbd/elements/button/GotoSignInOrUpMenu/GotoSignInOrUpMenu.tsx
@@ -12,7 +12,6 @@ import {KeyboardArrowDown, Login} from '@mui/icons-material';
 import {useIntl} from 'react-intl';
 import {getSSOLoginUrl} from '../../../common/SSOConfig';
 import {ButtonProps} from '@mui/material/Button/Button';
-import {getCDAPLoginUrl} from '../../../common/CDAPConfig';
 import Divider from '../../../components/Divider/Divider';
 
 interface Props extends ButtonProps {
@@ -85,7 +84,7 @@ const GotoSignInOrUpMenu = ({onClick, buttonText, icon, ...extra}: Props) => {
                 <ListItemText>{messages['common.login']}</ListItemText>
               </MenuItem>
             </Link>
-            <Divider sx={{margin: '0 !important'}} />
+            {/*<Divider sx={{margin: '0 !important'}} />
             <Link href={getCDAPLoginUrl()}>
               <MenuItem>
                 <ListItemIcon>
@@ -93,7 +92,7 @@ const GotoSignInOrUpMenu = ({onClick, buttonText, icon, ...extra}: Props) => {
                 </ListItemIcon>
                 <ListItemText>{messages['common.cdap_login']}</ListItemText>
               </MenuItem>
-            </Link>
+            </Link>*/}
           </button>
         </Card>
       )}
