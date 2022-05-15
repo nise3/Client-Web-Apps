f3a3e9694fa67e8849c7e8584c94e28ee76598ce
ehsan munna <grmunnabd@gmail.com>
2022-05-15 15:56:25 +0600

    icon and header
    added icon for certificate and rename header

---
+++ @softbd/icons/IconCertificateTemplate.tsx
@@ -0,0 +1,11 @@
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import React from 'react';
+const CertificateTemplateIcon = () => {
+  return (
+    <>
+      <DocumentScannerIcon />
+    </>
+  );
+};
+
+export default React.memo(CertificateTemplateIcon);
