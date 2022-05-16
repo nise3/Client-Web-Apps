3c6a2037c5f919355e127f466230ea19e4b743d9
AfrarJahin <afrarjahin@gmail.com>
2022-05-16 11:45:04 +0600

    fix: erpl page title refactoring done

--- erpl/pages/index.tsx
+++ erpl/pages/index.tsx
@@ -1,9 +1,17 @@
 import ErplDefaultFrontPage from '../../@softbd/layouts/hoc/ErplDefaultFrontPage';
 import React from 'react';
 import asyncComponent from '../../@crema/utility/asyncComponent';
+import {useIntl} from 'react-intl';
+import PageMeta from '../../@crema/core/PageMeta';
 
 const Erpl = asyncComponent(() => import('../../modules/erpl'));
 
 export default ErplDefaultFrontPage(() => {
-  return <Erpl />;
+  const {messages} = useIntl();
+  return (
+    <>
+      <PageMeta title={messages['common.rpl'] as string} />
+      <Erpl />
+    </>
+  );
 });
