049189d42a26aa68d0bb66adc3f23b7421132bd7
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2022-05-22 17:05:42 +0600

    simple refactoring

--- modules/routesConfig.tsx
+++ modules/routesConfig.tsx
@@ -668,6 +668,15 @@ const routesConfig: NavItemProps[] = [
         icon: 'person',
         children: [
           {
+            id: '4ir_occupations',
+            title: '4IR Occupations',
+            messageId: 'menu.occupations',
+            permissionKey: 'view_any_4ir_occupations',
+            type: 'item',
+            icon: 'work',
+            url: '/4ir-occupations',
+          },
+          {
             id: '4ir_guideline',
             title: '4IR Guideline',
             messageId: 'menu.guideline',
@@ -684,15 +693,6 @@ const routesConfig: NavItemProps[] = [
             type: 'item',
             icon: 'work',
             url: '/4ir-tagline',
-          },
-          {
-            id: '4ir_occupations',
-            title: '4IR Occupations',
-            messageId: 'menu.occupations',
-            permissionKey: 'view_any_4ir_occupations',
-            type: 'item',
-            icon: 'work',
-            url: '/4ir-occupations',
           },
           {
             id: '4ir_contributions',
