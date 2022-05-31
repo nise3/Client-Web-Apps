4df306ee6d879ffeb98155e182349978389a2228
Asir053 <asirbillah@gmail.com>
2022-05-31 12:32:26 +0600

    fixed sromo kalyan labor atashe page

--- migration-portal/pages/sromo-kalyan-atashe/index.tsx
+++ migration-portal/pages/sromo-kalyan-atashe/index.tsx
@@ -1,11 +1,11 @@
-import InstituteDefaultFrontPage from '../../../@softbd/layouts/hoc/InstituteDefaultFrontPage';
 import asyncComponent from '../../../@crema/utility/asyncComponent';
 import PageMeta from '../../../@crema/core/PageMeta';
 import {useIntl} from 'react-intl';
+import MigrationPortalDefaultFrontPage from '../../../@softbd/layouts/hoc/MigrationPortalDefaultFrontPage';
 
 const StaticContent = asyncComponent(() => import('../../../modules/sc'));
 
-export default InstituteDefaultFrontPage(() => {
+export default MigrationPortalDefaultFrontPage(() => {
   const {messages} = useIntl();
   return (
     <>
